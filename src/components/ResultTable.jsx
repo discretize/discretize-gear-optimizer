18508b378e35186d788f9e3ba6bc5b88b7bd5db1
gw2princeps <21026467+gw2princeps@users.noreply.github.com>
2021-08-01 09:56:46 +0200

    feat: improve result table optics

--- src/components/ResultTable.jsx
+++ src/components/ResultTable.jsx
@@ -1,28 +1,29 @@
-import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import Paper from '@material-ui/core/Paper';
-import Table from '@material-ui/core/Table';
-import TableBody from '@material-ui/core/TableBody';
-import TableCell from '@material-ui/core/TableCell';
-import TableContainer from '@material-ui/core/TableContainer';
-import TableHead from '@material-ui/core/TableHead';
-import TableRow from '@material-ui/core/TableRow';
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Paper from "@material-ui/core/Paper";
+import Table from "@material-ui/core/Table";
+import TableBody from "@material-ui/core/TableBody";
+import TableCell from "@material-ui/core/TableCell";
+import TableContainer from "@material-ui/core/TableContainer";
+import TableHead from "@material-ui/core/TableHead";
+import TableRow from "@material-ui/core/TableRow";
 
 import { useSelector } from "react-redux";
-import { getList } from "../state/gearOptimizerSlice";
+import { getGeneric, getList } from "../state/gearOptimizerSlice";
+import { Slots } from "../utils/gw2-data";
 
 const useStyles = makeStyles({
   root: {
-    width: '100%',
+    width: "100%"
   },
   container: {
-    maxHeight: 440,
-  },
+    maxHeight: 440
+  }
 });
 
 export default function StickyHeadTable() {
   const classes = useStyles();
-
+  const wield = useSelector(getGeneric("weaponType"));
   const list = useSelector(getList) || [];
 
   return (
@@ -31,28 +32,25 @@ export default function StickyHeadTable() {
         <Table stickyHeader aria-label="sticky table">
           <TableHead>
             <TableRow>
-            <TableCell>Damage</TableCell>
-            <TableCell align="right">Helm</TableCell>
-            <TableCell align="right">Shld</TableCell>
-            <TableCell align="right">Coat</TableCell>
-            <TableCell align="right">etc</TableCell>
-            <TableCell align="right">etc</TableCell>
-            <TableCell align="right">etc</TableCell>
-            <TableCell align="right">etc</TableCell>
-            <TableCell align="right">etc</TableCell>
-            <TableCell align="right">etc</TableCell>
+              <TableCell>Damage</TableCell>
+              {Slots[wield].map((slot) => (
+                <TableCell key={slot.name} align="center" padding="none">
+                  {slot.short}
+                </TableCell>
+              ))}
             </TableRow>
           </TableHead>
           <TableBody>
-            {list.map(character => (
+            {list.map((character) => (
               <TableRow key={character.id}>
                 <TableCell component="th" scope="row">
-                  {character.attributes['Damage'].toFixed(2)}
+                  {character.attributes["Damage"].toFixed(2)}
                 </TableCell>
                 {character.gear.map((element, index) => (
-                  <TableCell align="right" key={index}>{element}</TableCell>
-                ))
-                }
+                  <TableCell align="center" key={index} padding="none">
+                    {element.slice(0, 4)}
+                  </TableCell>
+                ))}
               </TableRow>
             ))}
           </TableBody>
