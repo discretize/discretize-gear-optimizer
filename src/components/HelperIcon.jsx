201a6a6a56688d5504b3fb145ddd4dd41c5215e6
Marcus <marcustyphoon@gmail.com>
2021-08-01 14:27:09 -0700

    Update HelperIcon.jsx

--- src/components/HelperIcon.jsx
+++ src/components/HelperIcon.jsx
@@ -1,12 +1,12 @@
-import { Typography, withStyles } from "@material-ui/core";
-import { HelpOutline } from "@material-ui/icons";
-import { Tooltip } from "gw2-ui";
-import React from "react";
+import { withStyles } from '@material-ui/core';
+import { HelpOutline } from '@material-ui/icons';
+import { Tooltip } from 'gw2-ui';
+import React from 'react';
 
 const styles = (theme) => ({
   icon: {
-    color: theme.palette.primary.dark
-  }
+    color: theme.palette.primary.dark,
+  },
 });
 
 const HelperIcon = ({ classes, text, size }) => {
