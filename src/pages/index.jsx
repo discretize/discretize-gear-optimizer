319090abddf2aec8af494cdaae956cde41e3ed0d
gw2princeps <21026467+gw2princeps@users.noreply.github.com>
2021-08-26 21:28:26 +0200

    feat: added beta warning

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,8 +1,10 @@
 import * as React from 'react';
 
-import { Typography, withStyles } from '@material-ui/core';
+import { Link, Typography, withStyles } from '@material-ui/core';
+import MuiAlert from '@material-ui/lab/Alert';
 import GearOptimizer from '../components/GearOptimizer';
 import withLayout from '../hocs/withLayout';
+import GitHubIcon from '@material-ui/icons/GitHub';
 
 const styles = (theme) => ({
   headline: {
@@ -14,6 +16,17 @@ const styles = (theme) => ({
 const IndexPage = ({ classes }) => {
   return (
     <>
+      <MuiAlert elevation={6} variant="filled" severity="warning">
+        The gear optimizer is currently in beta! Please report potential issues to us in{' '}
+        <Link href="https://discord.gg/Qdt7nFY" color="black">
+          Discord
+        </Link>{' '}
+        or{' '}
+        <Link href="https://github.com/discretize/discretize-gear-optimizer" color="black">
+          <GitHubIcon fontSize="small" />
+          Github
+        </Link>
+      </MuiAlert>
       <Typography variant="h2" className={classes.headline}>
         Gear Optimizer 2.0
       </Typography>
