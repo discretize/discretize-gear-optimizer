963222d59002955e85551235f1a44c680c5c0508
gw2princeps <21026467+gw2princeps@users.noreply.github.com>
2021-08-26 12:22:01 +0200

    fix: fouc duze to missing gatsby-plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -11,6 +11,7 @@ module.exports = {
         path: `./src/assets/images`,
       },
     },
+    'gatsby-plugin-material-ui',
     'gatsby-plugin-theme-ui',
     `gatsby-plugin-image`,
     `gatsby-plugin-sharp`,
