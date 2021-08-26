50c3ae363f76ef00bb7c6a74f3941795a4ce1029
gw2princeps <21026467+gw2princeps@users.noreply.github.com>
2021-08-26 23:32:54 +0200

    feat: coloring of selected character column

--- src/components/results/ResultTable.jsx
+++ src/components/results/ResultTable.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
 import Table from '@material-ui/core/Table';
 import TableBody from '@material-ui/core/TableBody';
@@ -9,10 +8,11 @@ import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
 
 import { useSelector, useDispatch } from 'react-redux';
-import { getList, getPriority, changeControl } from '../../state/gearOptimizerSlice';
+import { withStyles } from '@material-ui/core';
+import { getList, getPriority, changeControl, getControl } from '../../state/gearOptimizerSlice';
 import { Slots } from '../../utils/gw2-data';
 
-const useStyles = makeStyles({
+const styles = (theme) => ({
   root: {
     width: '100%',
   },
@@ -24,47 +24,50 @@ const useStyles = makeStyles({
   },
 });
 
-export default function StickyHeadTable() {
-  const classes = useStyles();
+const StickyHeadTable = ({ classes }) => {
   const wield = useSelector(getPriority('weaponType'));
+  const selected = useSelector(getControl('selected'));
+
   const list = useSelector(getList) || [];
   const dispatch = useDispatch();
 
   return (
-    <Paper className={classes.root}>
-      <TableContainer className={classes.container}>
-        <Table stickyHeader aria-label="sticky table">
-          <TableHead>
-            <TableRow>
-              <TableCell>Damage</TableCell>
-              {Slots[wield].map((slot) => (
-                <TableCell key={slot.name} align="center" padding="none">
-                  {slot.short}
+    <TableContainer className={classes.container}>
+      <Table stickyHeader aria-label="sticky table">
+        <TableHead>
+          <TableRow>
+            <TableCell>Damage</TableCell>
+            {Slots[wield].map((slot) => (
+              <TableCell key={slot.name} align="center" padding="none">
+                {slot.short}
+              </TableCell>
+            ))}
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {list.map((character, i) => (
+            <TableRow
+              key={character.id}
+              onClick={(e) => dispatch(changeControl({ key: 'selected', value: i }))}
+              className={classes.cell}
+              selected={i === selected}
+              style={i === selected ? { backgroundColor: '#009a9b' } : null}
+            >
+              <TableCell component="th" scope="row">
+                {character.attributes.Damage.toFixed(2)}
+              </TableCell>
+              {character.gear.map((element, index) => (
+                // eslint-disable-next-line react/no-array-index-key
+                <TableCell align="center" key={element + index} padding="none">
+                  {element.slice(0, 4)}
                 </TableCell>
               ))}
             </TableRow>
-          </TableHead>
-          <TableBody>
-            {list.map((character, i) => (
-              <TableRow
-                key={character.id}
-                onClick={(e) => dispatch(changeControl({ key: 'selected', value: i }))}
-                className={classes.cell}
-              >
-                <TableCell component="th" scope="row">
-                  {character.attributes.Damage.toFixed(2)}
-                </TableCell>
-                {character.gear.map((element, index) => (
-                  // eslint-disable-next-line react/no-array-index-key
-                  <TableCell align="center" key={element + index} padding="none">
-                    {element.slice(0, 4)}
-                  </TableCell>
-                ))}
-              </TableRow>
-            ))}
-          </TableBody>
-        </Table>
-      </TableContainer>
-    </Paper>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
   );
-}
+};
+
+export default withStyles(styles)(StickyHeadTable);
