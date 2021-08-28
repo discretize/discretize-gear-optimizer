9485665407bd9d78af0c75d8df6a32e219b1470b
gw2princeps <21026467+gw2princeps@users.noreply.github.com>
2021-08-29 00:19:37 +0200

    feat: highlighting affixes in resulttable

--- src/components/sections/results/ResultTableRow.jsx
+++ src/components/sections/results/ResultTableRow.jsx
@@ -2,17 +2,21 @@ import React from 'react';
 import TableRow from '@material-ui/core/TableRow';
 import TableCell from '@material-ui/core/TableCell';
 import { useDispatch } from 'react-redux';
-import { withStyles } from '@material-ui/core';
-import { changeSelectedCharacter } from '../../../state/gearOptimizerSlice';
+import { Typography, withStyles } from '@material-ui/core';
+import { changeSelectedCharacter, getPriority } from '../../../state/gearOptimizerSlice';
+import { useSelector } from 'react-redux';
 
 const styles = (theme) => ({
   cell: {
     cursor: 'pointer',
   },
+  highlight: { fontWeight: 200, fontSize: '1rem' },
 });
 
 const ResultTableRow = React.memo(({ classes, character, selected }) => {
   const dispatch = useDispatch();
+  const affixes = useSelector(getPriority('affixes'));
+  console.log(affixes);
   return (
     <TableRow
       className={classes.cell}
@@ -25,7 +29,15 @@ const ResultTableRow = React.memo(({ classes, character, selected }) => {
       {character.gear.map((element, index) => (
         // eslint-disable-next-line react/no-array-index-key
         <TableCell align="center" key={element + index} padding="none">
-          {element.slice(0, 4)}
+          <Typography
+            style={
+              affixes[0] !== element.toUpperCase()
+                ? { fontWeight: 300, fontSize: '1rem', color: '#00cccc' }
+                : { fontWeight: 300, fontSize: '1rem' }
+            }
+          >
+            {element.slice(0, 4)}
+          </Typography>
         </TableCell>
       ))}
     </TableRow>
