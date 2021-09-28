971be91f1d23800a92fad9403a36bacbd8e1e44a
Marcus <marcustyphoon@gmail.com>
2021-09-28 02:13:55 -0700

    add index to modifier data

---
+++ src/assets/modifierdata/index.js
@@ -0,0 +1,32 @@
+import buffs from './buffs.yaml';
+import food from './food.yaml';
+import utility from './utility.yaml';
+import runes from './runes.yaml';
+import sigils from './sigils.yaml';
+
+import elementalist from './elementalist.yaml';
+import engineer from './engineer.yaml';
+import guardian from './guardian.yaml';
+import mesmer from './mesmer.yaml';
+import necromancer from './necromancer.yaml';
+import ranger from './ranger.yaml';
+import revenant from './revenant.yaml';
+import thief from './thief.yaml';
+import warrior from './warrior.yaml';
+
+export default {
+  buffs,
+  food,
+  utility,
+  runes,
+  sigils,
+  elementalist,
+  engineer,
+  guardian,
+  mesmer,
+  necromancer,
+  ranger,
+  revenant,
+  thief,
+  warrior,
+};
