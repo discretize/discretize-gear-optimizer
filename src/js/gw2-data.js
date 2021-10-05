467cdb609ef8abd5f8470a8d85cf5df641892c20
Marcus <marcustyphoon@gmail.com>
2021-10-05 14:34:24 -0700

    data: remove undocumented condi buff from omnipotion

--- src/js/gw2-data.js
+++ src/js/gw2-data.js
@@ -598,8 +598,7 @@ export const ForcedSlots = [
 ];
 
 export const Omnipotion = {
-  // Re: condi dmg from omnipot
-  // https://discordapp.com/channels/301270513093967872/370538919118503947/716949463423516713
+  // Condi dmg from omnipot has been removed
   multiplier: {
     'Effective Power': 0.15,
     'add: Effective Condition Damage': 0.15, // stacks additively
@@ -614,9 +613,6 @@ export const Omnipotion = {
     },
     Concentration: {
       'Agony Resistance': 1.5
-    },
-    'Condition Damage': {
-      'Condition Damage': 0.15 // undocumented condition damage stat bonus
     }
   }
 };
