128b36e6c683d145bdfbde33aee1632984560645
Marcus <marcustyphoon@gmail.com>
2021-10-21 01:38:07 -0700

    testmodifiers: use fake assert function

--- src/assets/modifierdata/testModifiers.js
+++ src/assets/modifierdata/testModifiers.js
@@ -4,7 +4,7 @@
 /* eslint-disable no-console */
 import fs from 'fs/promises';
 import path from 'path';
-import assert from 'assert';
+// import assert from 'assert';
 // eslint-disable-next-line import/no-extraneous-dependencies
 import yaml from 'js-yaml';
 import {
@@ -24,6 +24,13 @@ import specializationData from '../../utils/mapping/specializations.json';
 
 const directory = './src/assets/modifierdata/';
 
+const gentleAssert = (condition, message) => {
+  if (!condition) {
+    console.error(`❌ ${message} ❌`);
+    process.exitCode = 1;
+  }
+};
+
 const testModifiers = async () => {
   const files = (await fs.readdir(directory)).filter(
     (filename) => path.extname(filename) === '.yaml',
@@ -40,10 +47,14 @@ const testModifiers = async () => {
     try {
       data = yaml.load(fileData);
     } catch (e) {
-      assert(false, `err: ${fileName} is invalid YAML`);
+      gentleAssert(false, `err: ${fileName} is invalid YAML`);
+      continue;
     }
 
-    assert(Array.isArray(data), `err: ${fileName} is not an array (use dashes for sections!)`);
+    gentleAssert(
+      Array.isArray(data),
+      `err: ${fileName} is not an array (use dashes for sections!)`,
+    );
 
     const fileIds = new Set();
     const allGw2ids = new Set();
@@ -64,8 +75,8 @@ const testModifiers = async () => {
       }
 
       const { items } = section;
-      assert(items, `err: items in ${sectionName} is missing (make it [])`);
-      assert(
+      gentleAssert(items, `err: items in ${sectionName} is missing (make it [])`);
+      gentleAssert(
         Array.isArray(items),
         `err: items in ${sectionName} is not an array (use dashes for sections!)`,
       );
@@ -100,9 +111,12 @@ const testModifiers = async () => {
         checkNullRecursively(item);
 
         if (amountData) {
-          assert(typeof amountData.label === 'string', `err: missing amount label in ${id}`);
-          assert(typeof amountData.default === 'number', `err: missing amount default in ${id}`);
-          assert(
+          gentleAssert(typeof amountData.label === 'string', `err: missing amount label in ${id}`);
+          gentleAssert(
+            typeof amountData.default === 'number',
+            `err: missing amount default in ${id}`,
+          );
+          gentleAssert(
             typeof amountData.quantityEntered === 'number',
             `err: missing amount quantityEntered in ${id}`,
           );
@@ -113,9 +127,9 @@ const testModifiers = async () => {
 
         if (major_traits) {
           if (major_traits.includes(gw2id)) {
-            assert(!minor, `err: ${id} is mistakenly labelled minor!`);
+            gentleAssert(!minor, `err: ${id} is mistakenly labelled minor!`);
           } else if (minor_traits.includes(gw2id)) {
-            assert(minor, `err: ${id} should be labelled minor!`);
+            gentleAssert(minor, `err: ${id} should be labelled minor!`);
           } else {
             // eslint-disable-next-line no-lonely-if
             if (gw2id && sectionName !== 'Soulbeast')
@@ -124,28 +138,28 @@ const testModifiers = async () => {
         }
 
         if (minor && !subText && !amountData) {
-          assert(defaultEnabled, `err: minor ${id} should be defaultEnabled!`);
+          gentleAssert(defaultEnabled, `err: minor ${id} should be defaultEnabled!`);
         }
 
         if (section.id && gw2id) {
-          assert(
+          gentleAssert(
             !allGw2ids.has(gw2id) || subText || amountData,
             `missing subtext for same gw2id in ${id}`,
           );
           allGw2ids.add(gw2id);
         }
 
-        assert(
+        gentleAssert(
           typeof id === 'string' && id !== '',
           `err: invalid or missing item id in ${sectionName}`,
         );
 
-        assert(!fileIds.has(id), `err:file has duplicate id ${id}`);
+        gentleAssert(!fileIds.has(id), `err:file has duplicate id ${id}`);
         fileIds.add(id);
 
         // duplicate ids within all the extras or within all the traits will be merged
         const allIds = fileIsExtra ? allExtrasIds : allTraitIds;
-        assert(!allIds.has(id), `duplicate id ${id} from different file`);
+        gentleAssert(!allIds.has(id), `duplicate id ${id} from different file`);
         allIds.add(id);
 
         // if (fileName !== 'buffs.yaml' && typeof gw2id !== 'number') {
@@ -155,10 +169,10 @@ const testModifiers = async () => {
         if (fileName === 'buffs.yaml' && typeof type !== 'string')
           console.log(`❓ ${id} doesn't have a type`);
 
-        assert(typeof modifiers === 'object', `err: invalid or missing modifiers in ${id}`);
+        gentleAssert(typeof modifiers === 'object', `err: invalid or missing modifiers in ${id}`);
 
         const { damage, attributes, conversion, effect, ...otherModifiers } = modifiers;
-        assert(
+        gentleAssert(
           Object.keys(otherModifiers).length === 0,
           `err: invalid modifier type(s): ${Object.keys(otherModifiers)}`,
         );
@@ -181,14 +195,12 @@ const testModifiers = async () => {
       }
     }
   }
-
-  console.log('🎉 looks like no major errors 🎉');
 };
 
 function parseDamage(damage, id, amountData) {
   for (const [key, allPairs] of Object.entries(damage)) {
-    assert(allDamageKeys.includes(key), `invalid damage key ${key} in ${id}`);
-    assert(
+    gentleAssert(allDamageKeys.includes(key), `invalid damage key ${key} in ${id}`);
+    gentleAssert(
       Array.isArray(allPairs),
       `invalid value for ${key} in ${id} (use 'unknown' if you don't know add/mult!)`,
     );
@@ -202,8 +214,8 @@ function parseDamage(damage, id, amountData) {
       const [amount, mode] = allPairsMut.splice(0, 2);
 
       parsePercent(amount, key, id);
-      assert(allDamageModes.includes(mode), `invalid val ${allPairs} for ${key} in ${id}`);
-      assert(
+      gentleAssert(allDamageModes.includes(mode), `invalid val ${allPairs} for ${key} in ${id}`);
+      gentleAssert(
         key !== 'Critical Damage' || mode === 'unknown',
         `set mode unknown for critical damage for now`,
       );
@@ -214,7 +226,7 @@ function parseDamage(damage, id, amountData) {
 function parseAttributes(attributes, id, amountData) {
   for (const [key, allPairs] of Object.entries(attributes)) {
     if (allAttributePointKeys.includes(key)) {
-      assert(
+      gentleAssert(
         Array.isArray(allPairs),
         `invalid value for ${key} in ${id} (use 'unknown' if you don't know if it's converted!)`,
       );
@@ -228,7 +240,7 @@ function parseAttributes(attributes, id, amountData) {
         const [amount, mode] = allPairsMut.splice(0, 2);
 
         parseNumber(amount, key, id);
-        assert(
+        gentleAssert(
           allAttributePointModes.includes(mode),
           `invalid val ${allPairs} for ${key} in ${id}`,
         );
@@ -239,14 +251,14 @@ function parseAttributes(attributes, id, amountData) {
 
       parsePercent(allPairs, key, id);
     } else {
-      assert(false, `invalid attribute key ${key} in ${id}`);
+      gentleAssert(false, `invalid attribute key ${key} in ${id}`);
     }
   }
 }
 
 function parseConversion(conversion, id, amountData) {
   for (const [key, value] of Object.entries(conversion)) {
-    assert(
+    gentleAssert(
       allConversionDestinationKeys.includes(key),
       `invalid conversion destination ${key} in ${id}`,
     );
@@ -255,7 +267,7 @@ function parseConversion(conversion, id, amountData) {
       console.log(`❓ ${key} is a bad idea in an entry with an amount like ${id}`);
 
     for (const [source, amount] of Object.entries(value)) {
-      assert(
+      gentleAssert(
         allConversionSourceKeys.includes(source),
         `invalid conversion source ${source} in ${id}`,
       );
@@ -269,7 +281,7 @@ function parsePercent(value, key, id) {
   try {
     num = Number(value.replace('%', ''));
   } finally {
-    assert(
+    gentleAssert(
       typeof num === 'number' && !Number.isNaN(num),
       `invalid number ${value} for ${key} in ${id}`,
     );
@@ -279,10 +291,16 @@ function parsePercent(value, key, id) {
 }
 
 function parseNumber(value, key, id) {
-  assert(
+  gentleAssert(
     typeof value === 'number' && !Number.isNaN(value),
     `invalid number ${value} for ${key} in ${id}`,
   );
 }
 
-testModifiers().catch((e) => console.error(`❌ ${e.message} ❌`));
+testModifiers()
+  .catch((e) => gentleAssert(false, e.message))
+  .then(() => {
+    if (!process.exitCode) {
+      console.log('🎉 no major errors 🎉');
+    }
+  });
