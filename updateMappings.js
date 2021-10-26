1f49e0c1d5d17bd0e1e573f70b795213dbabc864
Marcus <marcustyphoon@gmail.com>
2021-10-26 10:11:34 -0700

    use cache control none in updatemappings
    [skip ci]

--- updateMappings.js
+++ updateMappings.js
@@ -27,9 +27,9 @@ const groupBy = function (xs, key) {
 function fetchSpecializations() {
   console.log('Fetching specializations...');
   // get item stats (affixes)
-  axios.get('https://api.guildwars2.com/v2/specializations').then((res) => {
+  axios.get('https://api.guildwars2.com/v2/specializations', { 'Cache-Control': 'no-cache' }).then((res) => {
     axios
-      .get(`https://api.guildwars2.com/v2/specializations?ids=${res.data}`)
+      .get(`https://api.guildwars2.com/v2/specializations?ids=${res.data}`, { 'Cache-Control': 'no-cache' })
       .then((response) => {
         const specs = response.data.map((spec) => {
           return {
@@ -63,14 +63,14 @@ function fetchSpecializations() {
 function fetchTraits() {
   console.log('Fetching traits...');
   // get item stats (affixes)
-  axios.get('https://api.guildwars2.com/v2/traits').then((res) => {
+  axios.get('https://api.guildwars2.com/v2/traits', { 'Cache-Control': 'no-cache' }).then((res) => {
     const arrayOfArrays = [];
     for (let i = 0; i < res.data.length; i += MAX_ITEMS_PER_REQUEST) {
       arrayOfArrays.push(res.data.slice(i, i + MAX_ITEMS_PER_REQUEST));
     }
 
     const requests = arrayOfArrays.map((arr) =>
-      axios.get(`https://api.guildwars2.com/v2/traits?ids=${arr}`),
+      axios.get(`https://api.guildwars2.com/v2/traits?ids=${arr}`, { 'Cache-Control': 'no-cache' }),
     );
     axios
       .all(requests)
@@ -108,14 +108,14 @@ function fetchTraits() {
 function fetchSkills() {
   console.log('Fetching skills...');
   // get item stats (affixes)
-  axios.get('https://api.guildwars2.com/v2/skills').then((res) => {
+  axios.get('https://api.guildwars2.com/v2/skills', { 'Cache-Control': 'no-cache' }).then((res) => {
     const arrayOfArrays = [];
     for (let i = 0; i < res.data.length; i += MAX_ITEMS_PER_REQUEST) {
       arrayOfArrays.push(res.data.slice(i, i + MAX_ITEMS_PER_REQUEST));
     }
 
     const requests = arrayOfArrays.map((arr) =>
-      axios.get(`https://api.guildwars2.com/v2/skills?ids=${arr}`),
+      axios.get(`https://api.guildwars2.com/v2/skills?ids=${arr}`, { 'Cache-Control': 'no-cache' }),
     );
     axios
       .all(requests)
@@ -152,9 +152,9 @@ function fetchSkills() {
 function fetchItemStats() {
   console.log('Fetching itemStats...');
   // get item stats (affixes)
-  axios.get('https://api.guildwars2.com/v2/itemstats').then((res) => {
+  axios.get('https://api.guildwars2.com/v2/itemstats', { 'Cache-Control': 'no-cache' }).then((res) => {
     axios
-      .get(`https://api.guildwars2.com/v2/itemstats?ids=${res.data}`)
+      .get(`https://api.guildwars2.com/v2/itemstats?ids=${res.data}`, { 'Cache-Control': 'no-cache' })
       .then((response) => {
         const itemStats = response.data;
         const grouped = groupBy(itemStats, 'name');
@@ -187,7 +187,7 @@ function fetchItems() {
   console.log('Fetching items...');
   // get item stats (affixes)
   axios
-    .get(`https://api.datawars2.ie/gw2/v1/item_data/json`)
+    .get(`https://api.datawars2.ie/gw2/v1/item_data/json`, { 'Cache-Control': 'no-cache' })
     .then((response) => {
       const items = [];
       response.data
