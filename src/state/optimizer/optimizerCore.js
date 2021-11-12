84fad89c75477b596fe45f0f2e3fc60dd66c934d
Marcus <marcustyphoon@gmail.com>
2021-11-11 20:24:00 -0800

    temp fix: set results on characters in list

--- src/state/optimizer/optimizerCore.js
+++ src/state/optimizer/optimizerCore.js
@@ -797,6 +797,8 @@ let uniqueIDCounter = 0;
 function insertCharacter(character) {
   const { settings, attributes, valid } = character;
 
+  character.results = { value: character.attributes[settings.rankby] };
+
   if (!valid || (worstScore && worstScore > attributes[settings.rankby])) {
     return;
   }
