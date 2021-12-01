965b94464b63c31508f339a5ce32ac2a2458c22b
Marcus <marcustyphoon@gmail.com>
2021-11-30 19:46:40 -0800

    Implement skill amounts

--- src/components/sections/skills/Skills.jsx
+++ src/components/sections/skills/Skills.jsx
@@ -3,8 +3,9 @@ import { useTranslation } from 'gatsby-plugin-react-i18next';
 import { Skill } from 'gw2-ui-bulk';
 import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { getSkills, toggleSkill } from '../../../state/slices/skills';
+import { getSkills, toggleSkill, setSkillAmount } from '../../../state/slices/skills';
 import CheckboxComponent from '../../baseComponents/CheckboxComponent';
+import AmountInput from '../../baseComponents/AmountInput';
 
 const styles = (theme) => ({
   text: {
@@ -32,33 +33,52 @@ const Skills = ({ classes, data }) => {
     dispatch(toggleSkill({ id, enabled: e.target.checked }));
   };
 
+  const handleAmountChange = (id) => (e) => {
+    dispatch(setSkillAmount({ id, amount: e.target.value }));
+  };
+
   if (!data || data.length < 1) {
     return t('This class does not appear to have skills with extra buffs');
   }
 
-  return data.map((skill) => (
-    <div key={skill.id}>
-      <CheckboxComponent
-        value={skill.id}
-        checked={Boolean(skillState[skill.id])}
-        className={classes.checkbox}
-        label={
-          <div className={classes.label}>
-            <Skill id={skill.gw2id} disableLink className={classes.skill} />
-            {skill.subText && (
-              <Typography className={classes.subText}>
-                {
-                  // i18next-extract-mark-context-next-line {{skillSubText}}
-                  t('skillSubText', { context: skill.subText })
-                }
-              </Typography>
-            )}
-          </div>
-        }
-        onChange={handleCheckboxChange(skill.id)}
-      />
-    </div>
-  ));
+  return data.map((skill) => {
+    const { id, gw2id, subText, amountData } = skill;
+    const enabled = Boolean(skillState[id]);
+    const amount = skillState[id]?.amount || '';
+    return (
+      <div key={id}>
+        <CheckboxComponent
+          value={id}
+          checked={enabled}
+          className={classes.checkbox}
+          label={
+            <div className={classes.label}>
+              <Skill id={gw2id} disableLink className={classes.skill} />
+              {subText && (
+                <Typography className={classes.subText}>
+                  {
+                    // i18next-extract-mark-context-next-line {{skillSubText}}
+                    t('skillSubText', { context: subText })
+                  }
+                </Typography>
+              )}
+            </div>
+          }
+          onChange={handleCheckboxChange(id)}
+        />
+        {amountData ? (
+          <AmountInput
+            placeholder={amountData.default}
+            // i18next-extract-mark-context-next-line {{amountLabel}}
+            label={t('amountLabel', { context: amountData.label })}
+            handleAmountChange={handleAmountChange(id)}
+            value={amount}
+            disabled={!enabled}
+          />
+        ) : null}
+      </div>
+    );
+  });
 };
 
 export default withStyles(styles)(Skills);
