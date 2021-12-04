b98fd4ee72943c05bfcc83757b6ee3ff4631bbf5
Marcus <marcustyphoon@gmail.com>
2021-12-03 23:23:27 -0800

    Copy JSX/styling from Traits.jsx (commit 2634d06)

--- src/components/sections/skills/Skills.jsx
+++ src/components/sections/skills/Skills.jsx
@@ -1,4 +1,4 @@
-import { Typography, withStyles } from '@material-ui/core';
+import { Typography, withStyles, Box } from '@material-ui/core';
 import { useTranslation } from 'gatsby-plugin-react-i18next';
 import { Skill } from 'gw2-ui-bulk';
 import React from 'react';
@@ -46,37 +46,41 @@ const Skills = ({ classes, data }) => {
     const enabled = Boolean(skillState[id]);
     const amount = skillState[id]?.amount || '';
     return (
-      <div key={id}>
-        <CheckboxComponent
-          value={id}
-          checked={enabled}
-          className={classes.checkbox}
-          label={
-            <div className={classes.label}>
-              <Skill id={gw2id} disableLink className={classes.skill} />
-              {subText && (
-                <Typography className={classes.subText}>
-                  {
-                    // i18next-extract-mark-context-next-line {{skillSubText}}
-                    t('skillSubText', { context: subText })
-                  }
-                </Typography>
-              )}
-            </div>
-          }
-          onChange={handleCheckboxChange(id)}
-        />
-        {amountData ? (
-          <AmountInput
-            placeholder={amountData.default}
-            // i18next-extract-mark-context-next-line {{amountLabel}}
-            label={t('amountLabel', { context: amountData.label })}
-            handleAmountChange={handleAmountChange(id)}
-            value={amount}
-            disabled={!enabled}
+      <Box key={id} justifyContent="space-between" display="flex" maxWidth="648px">
+        <Box>
+          <CheckboxComponent
+            value={id}
+            checked={enabled}
+            className={classes.checkbox}
+            label={
+              <div className={classes.label}>
+                <Skill id={gw2id} disableLink className={classes.skill} />
+                {subText && (
+                  <Typography className={classes.subText}>
+                    {
+                      // i18next-extract-mark-context-next-line {{skillSubText}}
+                      t('skillSubText', { context: subText })
+                    }
+                  </Typography>
+                )}
+              </div>
+            }
+            onChange={handleCheckboxChange(id)}
           />
+        </Box>
+        {amountData ? (
+          <Box>
+            <AmountInput
+              placeholder={amountData.default}
+              // i18next-extract-mark-context-next-line {{amountLabel}}
+              label={t('amountLabel', { context: amountData.label })}
+              handleAmountChange={handleAmountChange(id)}
+              value={amount}
+              disabled={!enabled}
+            />
+          </Box>
         ) : null}
-      </div>
+      </Box>
     );
   });
 };
