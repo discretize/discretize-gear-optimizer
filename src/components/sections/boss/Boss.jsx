bd325f3315b4b27d1ef857b2742489e218f81219
Marcus <marcustyphoon@gmail.com>
2021-12-07 01:41:04 -0800

    add preset marks to confusion

--- src/components/sections/boss/Boss.jsx
+++ src/components/sections/boss/Boss.jsx
@@ -18,8 +18,58 @@ import { parseBoss } from '../../../utils/usefulFunctions';
 const CommonEffect = React.memo(CommonEffectRaw);
 const Condition = React.memo(ConditionRaw);
 
+const confusionOptionLabels = {
+  '0.000': 'Golem',
+  '0.133': 'Adina',
+  '0.184': 'Deimos',
+  '0.207': 'KC',
+  '0.249': 'Dhuum',
+  '0.295': 'Samarog',
+  '0.312': 'Qadim',
+  '0.318': 'Matthias',
+  '0.322': 'Xera',
+  '0.326': 'Sabetha',
+  '0.342': 'Sloth',
+  '0.361': 'Qadim 2',
+  '0.369': 'Gorseval',
+  '0.392': 'MO',
+  '0.457': 'Sabir',
+  '0.481': 'VG',
+  '0.565': 'Cairn',
+  '0.714': 'SH',
+  '0.769': 'Nikare',
+  '0.826': 'Kenut',
+};
+const confusionOptions = Object.keys(confusionOptionLabels);
+const confusionMarks = [
+  {
+    value: 0,
+    label: 'Golem',
+  },
+  {
+    value: 0.318,
+    label: 'Matthias',
+  },
+  {
+    value: 0.4,
+    label: '',
+  },
+  {
+    value: 0.565,
+    label: 'Cairn',
+  },
+  {
+    value: 0.714,
+    label: 'SH',
+  },
+  {
+    value: 0.8,
+    label: 'Largos',
+  },
+];
+
 const useStyles = makeStyles((theme) => ({
-  slider: { marginLeft: 16, maxWidth: 300, marginBottom: 28 },
+  slider: { marginLeft: 16, /* maxWidth: 300, */ marginBottom: 28 },
 }));
 
 const Boss = () => {
@@ -70,9 +120,11 @@ const Boss = () => {
 
       <AmountInput
         endLabel="%"
-        handleAmountChange={(e) => dispatch(changeMovementUptime(e.target.value))}
+        handleAmountChange={(_e, value) => dispatch(changeMovementUptime(value))}
         value={movementUptimeString}
-        maxWidth={36}
+        maxWidth={180}
+        useAutoComplete
+        autoCompleteProps={{ options: [] }}
       />
       <Slider
         value={movementUptime}
@@ -93,16 +145,21 @@ const Boss = () => {
       </Typography>
       <AmountInput
         endLabel={t('/s')}
-        handleAmountChange={(e) => dispatch(changeAttackRate(e.target.value))}
+        handleAmountChange={(_e, value) => dispatch(changeAttackRate(value))}
         value={attackRateString}
-        maxWidth={36}
+        maxWidth={180}
+        useAutoComplete
+        autoCompleteProps={{
+          options: confusionOptions,
+          renderOption: (option) => `${option}: ${confusionOptionLabels[option]}`,
+        }}
       />
       <Slider
         value={attackRate}
         step={0.01}
         min={0}
-        max={1}
-        // marks={}
+        max={0.83}
+        marks={confusionMarks}
         onChange={(_e, value) => dispatch(changeAttackRate(String(value)))}
         valueLabelDisplay="auto"
         className={classes.slider}
