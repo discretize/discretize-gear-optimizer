c2b64b980896bde671c6f108e9772f477da726e0
Huijie Shi <454906468@qq.com>
2021-12-18 17:25:57 +0800

    fix webpack resolve configuration

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,11 +1,7 @@
-// eslint-disable-next-line no-unused-vars
-const webpack = require(`webpack`);
-const path = require('path');
-
 exports.onCreateWebpackConfig = ({ actions }) => {
   actions.setWebpackConfig({
     // for some reason the modules option is required, else the css breaks entirely.
-    resolve: { symlinks: false, modules: [path.resolve('./node_modules')] },
+    resolve: { symlinks: false },
   });
 };
 
