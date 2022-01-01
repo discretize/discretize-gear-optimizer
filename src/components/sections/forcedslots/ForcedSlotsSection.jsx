7a2ed546954a9d5029474f109f6564509724fd2e
Marcus <marcustyphoon@gmail.com>
2022-01-01 13:11:06 -0800

    add buttons to clear or copy forced slots

--- src/components/sections/forcedslots/ForcedSlotsSection.jsx
+++ src/components/sections/forcedslots/ForcedSlotsSection.jsx
@@ -1,10 +1,28 @@
+import { Button } from '@material-ui/core';
 import { Trans, useTranslation } from 'gatsby-plugin-react-i18next';
 import React from 'react';
+import { useDispatch, useStore } from 'react-redux';
+import { getSelectedCharacter } from '../../../state/slices/controlsSlice';
+import { changeAllForcedSlots, clearForcedSlots } from '../../../state/slices/forcedSlots';
 import Section from '../../baseComponents/Section';
 import ForcedSlots from './ForcedSlots';
 
 const ForcedSlotsSection = () => {
   const { t } = useTranslation();
+  const store = useStore();
+  const dispatch = useDispatch();
+
+  const handleCopy = () => {
+    const selectedCharacter = getSelectedCharacter(store.getState());
+    const currentSelectedSlots = selectedCharacter?.gear;
+    if (currentSelectedSlots) {
+      dispatch(changeAllForcedSlots(currentSelectedSlots));
+    }
+  };
+
+  const handleClear = () => {
+    dispatch(clearForcedSlots());
+  };
 
   return (
     <Section
@@ -17,6 +35,12 @@ const ForcedSlotsSection = () => {
             between multiple builds.
           </Trans>
         </p>
+      }
+      extraInfo={
+        <>
+          <Button onClick={handleClear}>Clear</Button>
+          <Button onClick={handleCopy}>Copy from selected character</Button>
+        </>
       }
     />
   );
