df28ef123ed35b3dbc4f780c057d00fc7f58a47f
Marcus <marcustyphoon@gmail.com>
2022-03-03 04:26:16 -0800

    add amount boxes to extras kinda

--- src/components/sections/extras/Extras.jsx
+++ src/components/sections/extras/Extras.jsx
@@ -1,5 +1,4 @@
 import { ConsumableEffect, Item } from '@discretize/gw2-ui-new';
-import { Grid } from '@mui/material';
 import { useTranslation } from 'gatsby-plugin-react-i18next';
 import React from 'react';
 import { extrasModifiers, extrasModifiersById } from '../../../assets/modifierdata';
@@ -9,49 +8,42 @@ const Extras = () => {
   const { t } = useTranslation();
 
   return (
-    <Grid container spacing={2}>
-      <Grid item xs={12} md={6}>
-        <GW2Select
-          name="Sigil1"
-          label={<Item id={24615} disableLink disableTooltip text={t('Sigil 1')} />}
-          modifierData={extrasModifiers.sigils}
-          modifierDataById={extrasModifiersById}
-        />
-      </Grid>
-      <Grid item xs={12} md={6}>
-        <GW2Select
-          name="Sigil2"
-          label={<Item id={24868} disableLink disableTooltip text={t('Sigil 2')} />}
-          modifierData={extrasModifiers.sigils}
-          modifierDataById={extrasModifiersById}
-        />
-      </Grid>
-      <Grid item xs={12} md={6}>
-        <GW2Select
-          name="Runes"
-          label={<Item id={24836} disableLink disableTooltip text={t('Rune')} />}
-          modifierData={extrasModifiers.runes}
-          modifierDataById={extrasModifiersById}
-        />
-      </Grid>
-      <Grid item md={6} />
-      <Grid item xs={12} md={6}>
-        <GW2Select
-          name="Nourishment"
-          label={<ConsumableEffect text={t('Nourishment')} name="Nourishment" />}
-          modifierData={extrasModifiers.food}
-          modifierDataById={extrasModifiersById}
-        />
-      </Grid>
-      <Grid item xs={12} md={6}>
-        <GW2Select
-          name="Enhancement"
-          label={<ConsumableEffect text={t('Enhancement')} name="Enhancement" />}
-          modifierData={extrasModifiers.utility}
-          modifierDataById={extrasModifiersById}
-        />
-      </Grid>
-    </Grid>
+    <>
+      <GW2Select
+        name="Sigil1"
+        label={<Item id={24615} disableLink disableTooltip text={t('Sigil 1')} />}
+        modifierData={extrasModifiers.sigils}
+        modifierDataById={extrasModifiersById}
+      />
+
+      <GW2Select
+        name="Sigil2"
+        label={<Item id={24868} disableLink disableTooltip text={t('Sigil 2')} />}
+        modifierData={extrasModifiers.sigils}
+        modifierDataById={extrasModifiersById}
+      />
+
+      <GW2Select
+        name="Runes"
+        label={<Item id={24836} disableLink disableTooltip text={t('Rune')} />}
+        modifierData={extrasModifiers.runes}
+        modifierDataById={extrasModifiersById}
+      />
+
+      <GW2Select
+        name="Nourishment"
+        label={<ConsumableEffect text={t('Nourishment')} name="Nourishment" />}
+        modifierData={extrasModifiers.food}
+        modifierDataById={extrasModifiersById}
+      />
+
+      <GW2Select
+        name="Enhancement"
+        label={<ConsumableEffect text={t('Enhancement')} name="Enhancement" />}
+        modifierData={extrasModifiers.utility}
+        modifierDataById={extrasModifiersById}
+      />
+    </>
   );
 };
 
--- src/components/sections/extras/GW2Select.jsx
+++ src/components/sections/extras/GW2Select.jsx
@@ -1,5 +1,6 @@
 import { Item } from '@discretize/gw2-ui-new';
 import {
+  Box,
   FormControl,
   Input,
   InputLabel,
@@ -13,10 +14,13 @@ import { Trans, useI18next, useTranslation } from 'gatsby-plugin-react-i18next';
 import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { changeExtra, getExtra } from '../../../state/slices/extras';
+import AmountInput from '../../baseComponents/AmountInput';
 
 const GW2Select = ({ name, label, modifierData, modifierDataById }) => {
   const dispatch = useDispatch();
   const bigValue = useSelector(getExtra(name));
+
+  const { amountData } = modifierDataById[bigValue] || {};
 
   const { language } = useI18next();
   const { t } = useTranslation();
@@ -30,77 +34,93 @@ const GW2Select = ({ name, label, modifierData, modifierDataById }) => {
   // return an array in the select: https://github.com/mui-org/material-ui/issues/16181
   // Fragments are not supported as children!
   return (
-    <FormControl sx={{ width: '100%' }} variant="standard">
-      <InputLabel htmlFor={name}>{label}</InputLabel>
-      <Select
-        value={bigValue || ''}
-        input={<Input name={name} id={name} />}
-        onChange={handleChange}
-        renderValue={(selected) => {
-          const item = modifierDataById[selected];
-          return (
-            <Item
-              id={item.gw2id}
-              disableLink
-              {...(!isChinese && { text: item.text.replace('Superior ', '') })}
-              style={{ lineHeight: '1 !important' }}
-            />
-          );
-        }}
-      >
-        [
-        <MenuItem value="">
-          <em>
-            <Trans>None</Trans>
-          </em>
-        </MenuItem>
-        ,
-        {modifierData.map((category) => {
-          return [
-            <ListSubheader disableSticky>
-              {
-                // i18next-extract-mark-context-next-line {{extraSection}}
-                t('extraSection', { context: category.section })
-              }
-            </ListSubheader>,
-            category.items.map((item) => (
-              <MenuItem
-                key={item.id}
-                value={item.id}
-                sx={{
-                  whiteSpace: 'normal',
-                }}
-              >
-                <ListItemText
-                  primary={
-                    <Item
-                      id={item.gw2id}
-                      disableLink
-                      {...(!isChinese && { text: item.text.replace('Superior ', '') })}
-                    />
-                  }
-                  secondary={
-                    <Typography
-                      sx={{
-                        fontWeight: 100,
-                        fontSize: '0.8rem',
-                        textAlign: 'right',
-                      }}
-                    >
-                      {
-                        // i18next-extract-mark-context-next-line {{extraSubText}}
-                        t('extraSubText', { context: item.subText })
-                      }
-                    </Typography>
-                  }
+    <Box justifyContent="space-between" display="flex" maxWidth="648px" mb={2}>
+      <Box>
+        <FormControl sx={{ width: '100%' }} variant="standard">
+          <InputLabel htmlFor={name}>{label}</InputLabel>
+          <Select
+            value={bigValue || ''}
+            input={<Input name={name} id={name} />}
+            onChange={handleChange}
+            renderValue={(selected) => {
+              const item = modifierDataById[selected];
+              return (
+                <Item
+                  id={item.gw2id}
+                  disableLink
+                  {...(!isChinese && { text: item.text.replace('Superior ', '') })}
+                  style={{ lineHeight: '1 !important' }}
                 />
-              </MenuItem>
-            )),
-          ];
-        })}
-        ]
-      </Select>
-    </FormControl>
+              );
+            }}
+          >
+            [
+            <MenuItem value="">
+              <em>
+                <Trans>None</Trans>
+              </em>
+            </MenuItem>
+            ,
+            {modifierData.map((category) => {
+              return [
+                <ListSubheader disableSticky>
+                  {
+                    // i18next-extract-mark-context-next-line {{extraSection}}
+                    t('extraSection', { context: category.section })
+                  }
+                </ListSubheader>,
+                category.items.map((item) => (
+                  <MenuItem
+                    key={item.id}
+                    value={item.id}
+                    sx={{
+                      whiteSpace: 'normal',
+                    }}
+                  >
+                    <ListItemText
+                      primary={
+                        <Item
+                          id={item.gw2id}
+                          disableLink
+                          {...(!isChinese && { text: item.text.replace('Superior ', '') })}
+                        />
+                      }
+                      secondary={
+                        <Typography
+                          sx={{
+                            fontWeight: 100,
+                            fontSize: '0.8rem',
+                            textAlign: 'right',
+                          }}
+                        >
+                          {
+                            // i18next-extract-mark-context-next-line {{extraSubText}}
+                            t('extraSubText', { context: item.subText })
+                          }
+                        </Typography>
+                      }
+                    />
+                  </MenuItem>
+                )),
+              ];
+            })}
+            ]
+          </Select>
+        </FormControl>
+      </Box>
+      <Box>
+        {amountData ? (
+          <AmountInput
+            placeholder={amountData.default}
+            // i18next-extract-mark-context-next-line {{amountLabel}}
+            endLabel={t('amountLabel', { context: amountData.label })}
+            // handleAmountChange={handleAmountChange(id)}
+            // value={amount}
+            maxWidth={32}
+          />
+        ) : null}
+      </Box>
+    </Box>
   );
 };
 export default GW2Select;
