ec10b980f42ce75d4b5639d21f4e5363be738e8d
Marcus <marcustyphoon@gmail.com>
2022-03-09 12:17:50 -0800

    indent extras

--- src/components/sections/extras/Extras.jsx
+++ src/components/sections/extras/Extras.jsx
@@ -9,49 +9,51 @@ const Extras = () => {
   const { t } = useTranslation();
 
   return (
-    <Grid container spacing={2}>
-      <Grid item xs={12} md={6}>
-        <GW2Select
-          name="Sigil1"
-          label={<Item id={24615} disableLink disableTooltip text={t('Sigil 1')} />}
-          modifierData={extrasModifiers.sigils}
-          modifierDataById={allExtrasModifiersById}
-        />
+    <>
+      <Grid container spacing={2}>
+        <Grid item xs={12} md={6}>
+          <GW2Select
+            name="Sigil1"
+            label={<Item id={24615} disableLink disableTooltip text={t('Sigil 1')} />}
+            modifierData={extrasModifiers.sigils}
+            modifierDataById={allExtrasModifiersById}
+          />
+        </Grid>
+        <Grid item xs={12} md={6}>
+          <GW2Select
+            name="Sigil2"
+            label={<Item id={24868} disableLink disableTooltip text={t('Sigil 2')} />}
+            modifierData={extrasModifiers.sigils}
+            modifierDataById={allExtrasModifiersById}
+          />
+        </Grid>
+        <Grid item xs={12} md={6}>
+          <GW2Select
+            name="Runes"
+            label={<Item id={24836} disableLink disableTooltip text={t('Rune')} />}
+            modifierData={extrasModifiers.runes}
+            modifierDataById={allExtrasModifiersById}
+          />
+        </Grid>
+        <Grid item md={6} />
+        <Grid item xs={12} md={6}>
+          <GW2Select
+            name="Nourishment"
+            label={<ConsumableEffect disableLink text={t('Nourishment')} name="Nourishment" />}
+            modifierData={extrasModifiers.food}
+            modifierDataById={allExtrasModifiersById}
+          />
+        </Grid>
+        <Grid item xs={12} md={6}>
+          <GW2Select
+            name="Enhancement"
+            label={<ConsumableEffect disableLink text={t('Enhancement')} name="Enhancement" />}
+            modifierData={extrasModifiers.utility}
+            modifierDataById={allExtrasModifiersById}
+          />
+        </Grid>
       </Grid>
-      <Grid item xs={12} md={6}>
-        <GW2Select
-          name="Sigil2"
-          label={<Item id={24868} disableLink disableTooltip text={t('Sigil 2')} />}
-          modifierData={extrasModifiers.sigils}
-          modifierDataById={allExtrasModifiersById}
-        />
-      </Grid>
-      <Grid item xs={12} md={6}>
-        <GW2Select
-          name="Runes"
-          label={<Item id={24836} disableLink disableTooltip text={t('Rune')} />}
-          modifierData={extrasModifiers.runes}
-          modifierDataById={allExtrasModifiersById}
-        />
-      </Grid>
-      <Grid item md={6} />
-      <Grid item xs={12} md={6}>
-        <GW2Select
-          name="Nourishment"
-          label={<ConsumableEffect disableLink text={t('Nourishment')} name="Nourishment" />}
-          modifierData={extrasModifiers.food}
-          modifierDataById={allExtrasModifiersById}
-        />
-      </Grid>
-      <Grid item xs={12} md={6}>
-        <GW2Select
-          name="Enhancement"
-          label={<ConsumableEffect disableLink text={t('Enhancement')} name="Enhancement" />}
-          modifierData={extrasModifiers.utility}
-          modifierDataById={allExtrasModifiersById}
-        />
-      </Grid>
-    </Grid>
+    </>
   );
 };
 
