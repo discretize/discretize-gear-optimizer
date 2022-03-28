2ca003e1d71ad31396245140e74f980486945e46
gw2princeps <21026467+gw2princeps@users.noreply.github.com>
2022-03-29 00:12:19 +0200

    fix: adjust icon for Info

--- src/components/baseComponents/Info.jsx
+++ src/components/baseComponents/Info.jsx
@@ -1,4 +1,4 @@
-import LiveHelpIcon from '@mui/icons-material/LiveHelp';
+import WarningAmberIcon from '@mui/icons-material/WarningAmber';
 import { Box, Paper, Typography } from '@mui/material';
 import React from 'react';
 
@@ -7,7 +7,7 @@ export default function Info({ children, direction = 'column' }) {
     <Paper sx={{ mt: 0.5, mb: 1 }} elevation={0}>
       <Box sx={{ p: 1, display: 'flex', flexDirection: direction }}>
         <Box sx={{ mr: 1 }}>
-          <LiveHelpIcon />
+          <WarningAmberIcon />
         </Box>
         <Typography variant="caption" paragraph sx={{ mb: 0 }}>
           {children}
