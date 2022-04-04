63cbd9fdd169e3d5bf5dd8521c5772a28b154553
gw2princeps <21026467+gw2princeps@users.noreply.github.com>
2022-04-04 22:29:28 +0200

    fix: hide background image on smaller devices

--- src/components/baseComponents/BackgroundImage.jsx
+++ src/components/baseComponents/BackgroundImage.jsx
@@ -1,11 +1,17 @@
+import { useMediaQuery } from '@mui/material';
+import { useTheme } from '@mui/system';
 import React from 'react';
 import { useSelector } from 'react-redux';
 import fractalImg from '../../assets/images/background/fractals.jpg';
 import raidImg from '../../assets/images/background/raids.jpg';
 import { getGameMode } from '../../state/slices/controlsSlice';
+
 export default function BackgroundImage() {
   const gameMode = useSelector(getGameMode);
-
+  const theme = useTheme();
+  const showImage = useMediaQuery(theme.breakpoints.up('lg'));
+  // only show the image for larger screens
+  if (!showImage) return null;
   return (
     <img
       src={gameMode === 'fractals' ? fractalImg : raidImg}
@@ -21,6 +27,7 @@ export default function BackgroundImage() {
       }}
       loading="lazy"
       decoding="async"
+      alt="Background"
     />
   );
 }
