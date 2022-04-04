9245621a53fb1cae76c1b57f977da44a72de37c7
Marcus <marcustyphoon@gmail.com>
2022-04-04 01:26:22 -0700

    Progress on typing modifier data
    [skip ci]

--- src/assets/modifierdata/metadata.ts
+++ src/assets/modifierdata/metadata.ts
@@ -10,6 +10,7 @@ const stats = [
   'Healing Power',
   'Agony Resistance',
 ] as const;
+export type StatName = typeof stats[number];
 
 export const boons = [
   'Aegis',
@@ -25,9 +26,9 @@ export const boons = [
   'Swiftness',
   'Vigor',
 ] as const;
-type Boon = typeof boons[number];
+export type BoonName = typeof boons[number];
 
-const boonDurations = boons.map((boon) => `${boon} Duration`) as `${Boon} Duration`[];
+const boonDurations = boons.map((boon) => `${boon} Duration`) as `${BoonName} Duration`[];
 
 // const conditions = [
 //   'Bleeding',
@@ -52,19 +53,19 @@ export const damagingConditions = [
   'Poison',
   'Torment',
 ] as const;
-type DamagingCondition = typeof damagingConditions[number];
+export type DamagingConditionName = typeof damagingConditions[number];
 
 const conditionDurations = damagingConditions.map(
   (condition) => `${condition} Duration`,
-) as `${DamagingCondition} Duration`[];
+) as `${DamagingConditionName} Duration`[];
 
 const conditionCoefficients = damagingConditions.map(
   (condition) => `${condition} Coefficient`,
-) as `${DamagingCondition} Coefficient`[];
+) as `${DamagingConditionName} Coefficient`[];
 
 const conditionDamages = damagingConditions.map(
   (condition) => `${condition} Damage`,
-) as `${DamagingCondition} Damage`[];
+) as `${DamagingConditionName} Damage`[];
 
 export const percents = [
   'Critical Chance',
@@ -92,16 +93,28 @@ export const allDamageKeys = [
   'Critical Damage',
   ...conditionDamages,
 ] as const;
+type DamageKey = typeof allDamageKeys[number];
+
 export const allDamageModes = ['add', 'mult', 'target', 'unknown'] as const;
+type DamageMode = typeof allDamageModes[number];
 
 export const allAttributePointKeys = stats;
+type AttributePointKey = typeof allAttributePointKeys[number];
+
 export const allAttributePointModes = ['buff', 'converted', 'unknown'] as const;
+type AttributePointMode = typeof allAttributePointModes[number];
 
 export const allAttributeCoefficientKeys = coefficients;
+type AttributeCoefficientKey = typeof allAttributeCoefficientKeys[number];
 export const allAttributePercentKeys = percents;
+type AttributePercentKey = typeof allAttributePercentKeys[number];
+
+type AttributeKey = AttributePointKey | AttributeCoefficientKey | AttributePercentKey;
 
 export const allConversionSourceKeys = stats;
+type ConversionSourceKey = typeof allConversionSourceKeys[number];
 export const allConversionDestinationKeys = [...stats, ...percents, ...coefficients] as const;
+type ConversionDestinationKey = typeof allConversionDestinationKeys[number];
 
 export const allConversionAfterBuffsSourceKeys = [
   ...stats,
@@ -207,3 +220,51 @@ export const exampleModifiersJson = `{
     "Outgoing Healing": { "Healing Power": "0.006%" }
   }
 }`;
+
+type Percent = string;
+
+type DamageValue = [Percent, DamageMode, Percent?, DamageMode?];
+type DamageModifiers = Partial<Record<DamageKey, DamageValue>>;
+
+type AttributeModifiers = {
+  [Key in AttributeKey]?: Key extends AttributePointKey
+    ? [number, AttributePointMode]
+    : Key extends AttributeCoefficientKey
+    ? number
+    : Key extends AttributePercentKey
+    ? Percent
+    : never;
+};
+
+type ConversionValue = Partial<Record<ConversionSourceKey, Percent>>;
+type ConversionModifers = Partial<Record<ConversionDestinationKey, ConversionValue>>;
+
+export interface Modifiers {
+  damage?: DamageModifiers;
+  attributes?: AttributeModifiers;
+  conversion?: ConversionModifers;
+  conversionAfterBuffs?: ConversionModifers;
+}
+
+const test: Modifiers = {
+  'damage': {
+    'Strike Damage': ['10%', 'mult'],
+    'All Damage': ['20%', 'add'],
+    'Critical Damage': ['20%', 'unknown'],
+    'Burning Damage': ['33%', 'unknown'],
+  },
+  'attributes': {
+    'Ferocity': [100, 'converted'],
+    'Healing Power': [30, 'buff'],
+    'Condition Damage': [60, 'converted'],
+    'Critical Chance': '20%',
+    'Quickness Duration': '10%',
+    'Torment Duration': '25%',
+    'Outgoing Healing': '5%',
+  },
+  'conversion': {
+    'Power': { 'Precision': '3%', 'Ferocity': '6%' },
+    'Expertise': { 'Toughness': '3%' },
+    'Outgoing Healing': { 'Healing Power': '0.006%' },
+  },
+};
