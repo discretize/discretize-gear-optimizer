83825ad6a42eee251ef2f512c6d4c465721cb171
Marcus <marcustyphoon@gmail.com>
2022-04-12 16:21:33 -0700

    Revert "Remove breakpoint from BackgroundImage"
    [skip ci]

--- src/components/baseComponents/BackgroundImage.jsx
+++ src/components/baseComponents/BackgroundImage.jsx
@@ -1,8 +1,14 @@
+import { useMediaQuery, useTheme } from '@mui/material';
 import React from 'react';
 import fractalImg from '../../assets/images/background/fractals.jpg';
 import raidImg from '../../assets/images/background/raids.jpg';
 
 export default function BackgroundImage({ gameMode }) {
+  const theme = useTheme();
+  const showImage = useMediaQuery(theme.breakpoints.up('lg'));
+  // only show the image for larger screens
+  if (!showImage) return null;
+
   return (
     <img
       src={gameMode === 'fractals' ? fractalImg : raidImg}
