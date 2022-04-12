45b559f9a599bdeaf39b83358cede062dfb93127
Marcus <marcustyphoon@gmail.com>
2022-04-12 14:36:50 -0700

    Refactor: Info component

--- src/components/baseComponents/Info.jsx
+++ src/components/baseComponents/Info.jsx
@@ -1,15 +1,12 @@
-import WarningAmberIcon from '@mui/icons-material/WarningAmber';
 import { Box, Paper, Typography } from '@mui/material';
 import React from 'react';
 
-export default function Info({ children, direction = 'column' }) {
+export default function Info({ children, direction = 'row', variant = 'caption', icon }) {
   return (
     <Paper sx={{ mt: 0.5, mb: 1 }} elevation={0}>
       <Box sx={{ p: 1, display: 'flex', flexDirection: direction }}>
-        <Box sx={{ mr: 1 }}>
-          <WarningAmberIcon />
-        </Box>
-        <Typography variant="caption" paragraph sx={{ mb: 0 }}>
+        <Box sx={{ mr: 1 }}>{icon}</Box>
+        <Typography variant={variant} paragraph sx={{ mb: 0 }}>
           {children}
         </Typography>
       </Box>
