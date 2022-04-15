e2b1953aa5ab65042e8e6bfe89d15e1b7e2bbc1c
Marcus <marcustyphoon@gmail.com>
2022-04-15 15:12:30 -0700

    Refactor URL export

--- src/components/url-state/URLStateExport.jsx
+++ src/components/url-state/URLStateExport.jsx
@@ -1,127 +1,11 @@
 import { Tooltip } from '@discretize/gw2-ui-new';
 import ShareIcon from '@mui/icons-material/Share';
 import { IconButton } from '@mui/material';
-import axios from 'axios';
 import React from 'react';
 import { useTranslation } from 'react-i18next';
 import { useDispatch } from 'react-redux';
 import SagaTypes from '../../state/sagas/sagaTypes';
-import { PARAMS } from '../../utils/queryParam';
 import URLStateSnackbar from './URLStateSnackbar';
-
-// hard coded temporarily!
-const version = 0;
-
-const cloudflare = (longUrl, binaryData, setSnackbarState, setLoading, t) => {
-  const shortenPromise = axios.post(`share/create`, binaryData).then((res) => {
-    if (res?.data?.Status === 200) {
-      const { Key } = res.data;
-
-      const urlObject = new URL(window.location.href);
-      urlObject.searchParams.set(PARAMS.SHORTENER, `${Key}v1`);
-      const shortUrl = urlObject.href;
-
-      console.log('Exported short URL:', shortUrl);
-      return shortUrl;
-    }
-    console.log(`URL shortener returned status ${res?.data?.Status}!`);
-    return longUrl;
-  });
-  const timeoutPromise = new Promise((resolve) => setTimeout(resolve, 3000, longUrl));
-
-  const urlPromise = Promise.any([shortenPromise, timeoutPromise]);
-  const urlBlobPromise = urlPromise.then((url) => new Blob([url], { type: 'text/plain' }));
-
-  // iOS browsers and desktop Safari require the use of the async clipboard API, calling
-  // navigator.clipboard.write synchronously and passing it a Promise
-  // (see: https://web.dev/async-clipboard/).
-  // Firefox does not support this API but allows writing to the clipboard in a callback.
-  // Chrome doesn't care.
-  const clipboardPromise =
-    typeof ClipboardItem !== 'undefined'
-      ? // eslint-disable-next-line no-undef
-        navigator.clipboard.write([new ClipboardItem({ 'text/plain': urlBlobPromise })])
-      : urlPromise.then((url) => navigator.clipboard.writeText(url));
-
-  clipboardPromise
-    .then(() =>
-      setSnackbarState((state) => ({
-        ...state,
-        open: true,
-        success: true,
-        message: t('Copied link to clipboard!'),
-      })),
-    )
-    .catch(() =>
-      setSnackbarState((state) => ({
-        ...state,
-        open: true,
-        success: true,
-        message: t('Failed to copy link to clipboard!'),
-      })),
-    )
-    .finally(() => {
-      setLoading(false);
-    });
-};
-
-// TODO remove once the optimizer is on cloudflare exclusively
-/*
-const oldShortener = (longUrl, setSnackbarState, setLoading, t) => {
-  // get request to create a new short-url
-  // this url points to a cloudflare worker, which acts as a url shortener
-  // Source for the shortener: https://gist.github.com/gw2princeps/dc88d11e6b2378db35bcb2dd3726c7c6
-  const shortenPromise = axios
-    .get(`https://go.princeps.biz/?new=${longUrl.replaceAll('&', '%26')}`)
-    .then((res) => {
-      if (res?.data?.Status === 200) {
-        console.log('Exported short URL:', res.data.ShortUrl);
-        return res.data.ShortUrl;
-      }
-      console.log(`URL shortener returned status ${res?.data?.Status}!`);
-      return longUrl;
-    });
-  const timeoutPromise = new Promise((resolve) => setTimeout(resolve, 3000, longUrl));
-
-  const urlPromise = Promise.any([shortenPromise, timeoutPromise]);
-  const urlBlobPromise = urlPromise.then((url) => new Blob([url], { type: 'text/plain' }));
-
-  // iOS browsers and desktop Safari require the use of the async clipboard API, calling
-  // navigator.clipboard.write synchronously and passing it a Promise
-  // (see: https://web.dev/async-clipboard/).
-  // Firefox does not support this API but allows writing to the clipboard in a callback.
-  // Chrome doesn't care.
-  const clipboardPromise =
-    typeof ClipboardItem !== 'undefined'
-      ? // eslint-disable-next-line no-undef
-        navigator.clipboard.write([new ClipboardItem({ 'text/plain': urlBlobPromise })])
-      : urlPromise.then((url) => navigator.clipboard.writeText(url));
-
-  clipboardPromise
-    .then(() =>
-      setSnackbarState((state) => ({
-        ...state,
-        open: true,
-        success: true,
-        message: t('Copied link to clipboard!'),
-      })),
-    )
-    .catch(() =>
-      setSnackbarState((state) => ({
-        ...state,
-        open: true,
-        success: true,
-        message: t('Failed to copy link to clipboard!'),
-      })),
-    )
-    .finally(() => {
-      setLoading(false);
-    });
-
-  // setBuildUrl would trigger an update in the useEffects method of URLState... which is not what we want
-  // setBuildUrl(data);
-};
-*/
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 const URLStateExport = ({ type }) => {
@@ -140,55 +24,24 @@ const URLStateExport = ({ type }) => {
   // URL state loading logic
   /// const [_, setBuildUrl] = useQueryParam('data', StringParam);
 
-  const onExportSuccess = React.useCallback(
-    (jsonUrlData, binaryData) => {
-      if (typeof window === 'undefined') return;
+  const onSuccess = React.useCallback((message) => {
+    setSnackbarState((state) => ({
+      ...state,
+      open: true,
+      success: true,
+      message,
+    }));
+    setLoading(false);
+  }, []);
 
-      const urlObject = new URL(window.location.href);
-      urlObject.searchParams.set(PARAMS.VERSION, version);
-      urlObject.searchParams.set(PARAMS.BUILD, jsonUrlData);
-      const longUrl = urlObject.href;
-
-      console.log(`Exported long URL (${longUrl.length} characters):`, longUrl);
-
-      if (import.meta.env.VITE_HAS_CF) {
-        cloudflare(longUrl, binaryData, setSnackbarState, setLoading, t);
-        return;
-      }
-
-      if (longUrl.length > 8000) {
-        console.log(`URL is too long! (${longUrl.length} characters):`, longUrl);
-        setSnackbarState((state) => ({
-          ...state,
-          open: true,
-          success: false,
-          message: t('Error: too much data!'),
-        }));
-        setLoading(false);
-        return;
-      }
-
-      setSnackbarState((state) => ({
-        ...state,
-        open: true,
-        success: true,
-        message: t('Copied long link to clipboard! (Link shortener requires cloudflare mode.)'),
-      }));
-      navigator.clipboard.writeText(longUrl);
-      setLoading(false);
-    },
-    [t],
-  );
-
-  const onLoadError = React.useCallback(() => {
+  const onFailure = React.useCallback((message) => {
     setSnackbarState((state) => ({
       ...state,
       open: true,
       success: false,
-      message: 'There was an error exporting the state!',
+      message,
     }));
     setLoading(false);
-    // console.log('An error occured!');
   }, []);
 
   return (
@@ -199,8 +52,9 @@ const URLStateExport = ({ type }) => {
             setLoading(true);
             dispatch({
               type: SagaTypes.ExportFormState,
-              onSuccess: onExportSuccess,
-              onError: onLoadError,
+              t,
+              onSuccess,
+              onFailure,
             });
           }}
           size="large"
--- src/state/sagas/formStateSaga.js
+++ src/state/sagas/formStateSaga.js
@@ -1,11 +1,16 @@
+import axios from 'axios';
 import JsonUrl from 'json-url';
 import pako from 'pako';
 import { all, put, select, takeLeading } from 'redux-saga/effects';
+import { PARAMS } from '../../utils/queryParam';
 // import { changeBuildPage } from '../slices/buildPage';
 import { changeAll } from '../slices/controlsSlice';
 import SagaTypes from './sagaTypes';
 
 const lib = JsonUrl('lzma');
+
+// hard coded temporarily!
+const version = 0;
 
 const modifyState = (optimizerState) => {
   // const list = optimizerState?.control?.list.slice(0, 6) || [];
@@ -75,26 +80,88 @@ const unModifyState = (importData) => {
   return optimizerState;
 };
 
-function* exportState({ onSuccess, onError }) {
+function* exportState({ t, onSuccess, onFailure }) {
+  if (typeof window === 'undefined') return;
   try {
     const reduxState = yield select();
 
     const exportData = modifyState(reduxState.optimizer);
     console.log(exportData);
 
-    console.time('Created template in:');
-    const jsonUrlCompressed = yield lib.compress(exportData);
-    console.timeEnd('Created template in:');
+    let successMessage = t('Copied link to clipboard!');
 
-    console.time('Created binary template in:');
-    const binaryCompressed = pako.deflate(JSON.stringify(exportData));
-    console.timeEnd('Created binary template in:');
+    // navigator.clipboard.write() must be called synchronously and unconditionally.
+    // To avoid copying garbage to clipboard if there is an asynchronous error, we pass it an
+    // unresolved promise by simply never calling resolve() inside a Promise constructor if there
+    // is nothing to copy.
 
-    onSuccess(jsonUrlCompressed, binaryCompressed);
+    // eslint-disable-next-line no-async-promise-executor
+    const urlPromise = new Promise(async function createUrlPromise(resolve) {
+      // in cloudflare environments, use CF function URL shortener (see functions/share/create.ts)
+      if (import.meta.env.VITE_HAS_CF) {
+        console.time('Compressed binary data in:');
+        const binaryData = pako.deflate(JSON.stringify(exportData));
+        console.timeEnd('Compressed binary data in:');
+
+        const response = await axios.post(`share/create`, binaryData).catch(console.error);
+        if (response?.data?.Status === 200) {
+          const { Key } = response.data;
+
+          const urlObject = new URL(window.location.href);
+          urlObject.searchParams.set(PARAMS.SHORTENER, `${Key}v1`);
+          const shortUrl = urlObject.href;
+
+          console.log('Exported short URL:', shortUrl);
+          resolve(shortUrl);
+          return;
+        }
+        console.log(
+          `URL shortener returned status ${response?.data?.Status}! Falling back to long URL.`,
+        );
+        successMessage = t('Copied long link to clipboard! (Link shortener failed.)');
+      } else {
+        successMessage = t(
+          'Copied long link to clipboard! (Link shortener requires cloudflare environment.)',
+        );
+      }
+
+      // fall back to long URL
+      console.time('Compressed json-url data in:');
+      const jsonUrlData = await lib.compress(exportData);
+      console.timeEnd('Compressed json-url data in:');
+
+      const urlObject = new URL(window.location.href);
+      urlObject.searchParams.set(PARAMS.VERSION, version);
+      urlObject.searchParams.set(PARAMS.BUILD, jsonUrlData);
+      const longUrl = urlObject.href;
+
+      if (longUrl.length > 8000) {
+        console.log(`Long URL (${longUrl.length} characters) is too long:`, longUrl);
+        onFailure(t('Error: too much data!'));
+      } else {
+        resolve(longUrl);
+      }
+    });
+
+    // iOS browsers and desktop Safari require the use of the async clipboard API, calling
+    // navigator.clipboard.write synchronously and passing it a Promise
+    // (see: https://web.dev/async-clipboard/).
+    // Firefox does not support this API but allows writing to the clipboard in a callback.
+    // Chrome doesn't care.
+    const urlBlobPromise = urlPromise.then((url) => new Blob([url], { type: 'text/plain' }));
+    const clipboardPromise =
+      typeof ClipboardItem !== 'undefined'
+        ? // eslint-disable-next-line no-undef
+          navigator.clipboard.write([new ClipboardItem({ 'text/plain': urlBlobPromise })])
+        : urlPromise.then((url) => navigator.clipboard.writeText(url));
+
+    clipboardPromise
+      .then(() => onSuccess(successMessage))
+      .catch(() => onFailure(t('Failed to copy link to clipboard!')));
   } catch (e) {
     console.log('Problem saving and sharing state!');
     console.log(e);
-    onError();
+    onFailure(t('There was an error exporting the state!'));
   }
 }
 
