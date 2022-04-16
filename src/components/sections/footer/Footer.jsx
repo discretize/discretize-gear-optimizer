024f0885a8939a6ceb7c37e1e90ffd91b9e0256c
gw2princeps <21026467+gw2princeps@users.noreply.github.com>
2022-04-16 11:29:37 +0200

    styling: slight adjustment to footer

--- src/components/sections/footer/Footer.jsx
+++ src/components/sections/footer/Footer.jsx
@@ -3,6 +3,7 @@ import GitHubIcon from '@mui/icons-material/GitHub';
 import { Grid, Link, List, ListItem, ListItemIcon, ListItemText, Typography } from '@mui/material';
 import Discord from '../../../utils/discord';
 import Layout from '../../baseComponents/Layout';
+import { Trans } from 'react-i18next';
 
 const LINK_PROPS = {
   target: '_blank',
@@ -36,10 +37,12 @@ export default function Footer() {
     <Layout ContainerProps={{ sx: { marginTop: 2 } }}>
       <Grid container justifyContent="space-between">
         <Grid item>
-          <Typography variant="caption">Links:</Typography>
+          <Typography variant="body2" fontWeight={600}>
+            <Trans>Links</Trans>
+          </Typography>
           <List disablePadding dense>
             {LINKS.map(({ label, href, Icon }) => (
-              <ListItem>
+              <ListItem key={label} disablePadding>
                 <ListItemIcon sx={{ minWidth: 'unset' }}>
                   <Icon />
                 </ListItemIcon>
@@ -54,10 +57,12 @@ export default function Footer() {
         </Grid>
 
         <Grid item>
-          <Typography variant="caption">Contributors:</Typography>
+          <Typography variant="body2" fontWeight={600}>
+            Contributors
+          </Typography>
           <List disablePadding dense>
             {CONTRIBUTORS.map(({ label, href }) => (
-              <ListItem>
+              <ListItem key={label} disablePadding sx={{ paddingBottom: 0.2 }}>
                 <ListItemText>
                   <Link href={href} {...LINK_PROPS}>
                     {label}
