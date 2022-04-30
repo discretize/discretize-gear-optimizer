424b73fdd4e192ef3338fca4ba69fb7c91205bf8
gw2princeps <21026467+gw2princeps@users.noreply.github.com>
2022-04-30 10:03:37 +0200

    feat: headline and helptext for affixes

--- src/components/sections/affixes/Affixes.jsx
+++ src/components/sections/affixes/Affixes.jsx
@@ -1,5 +1,8 @@
+import { Typography } from '@mui/material';
 import React from 'react';
+import { HelperIcon } from '@discretize/react-discretize-components';
 import { useDispatch, useSelector } from 'react-redux';
+import { useTranslation } from 'react-i18next';
 import {
   changePriority,
   getExclusionsEnabled,
@@ -10,11 +13,22 @@ import ExcludedSlots from '../forcedslots/ExcludedSlots';
 
 export default function Affixes() {
   const dispatch = useDispatch();
+  const { t } = useTranslation();
+
   const affixes = useSelector(getPriority('affixes'));
   const exclusionsEnabled = useSelector(getExclusionsEnabled);
 
   return (
     <>
+      <Typography fontWeight={800} mb={0.5}>
+        {t('Desired Affixes')}{' '}
+        <HelperIcon
+          text={t(
+            'Affixes (stats) you are interested in. The optimizer will try out every combination and return the best results. More than three affixes might take a long time.',
+          )}
+          size="small"
+        />
+      </Typography>
       <AffixesSelect
         multiple
         onChange={(event, value) => {
