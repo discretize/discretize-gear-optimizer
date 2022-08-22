aac04aca2634ce79b1f3a930adf9d0a402d6ee64
Marcus <marcustyphoon@gmail.com>
2022-08-22 15:48:02 -0700

    Update alert message

--- src/pages/index/index.jsx
+++ src/pages/index/index.jsx
@@ -24,9 +24,9 @@ const IndexPage = () => {
 
   const ALERTS = [
     <Trans>
-      Core game changes are updated for the June 28 game patch, but preset coefficients and trait
-      selections are not yet updated. Most gear results will be correctly optimized, but DPS
-      estimates will be wrong.
+      Core game changes are updated for the June 28 and August 23 game patches, but preset
+      coefficients and trait selections are not yet updated. Most gear results will be correctly
+      optimized, but DPS estimates will be wrong.
       <br />
       <br />
       Only templates with * asterisks are currently generated from post-patch logs.
