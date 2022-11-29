07ca335befa9f5c02088fa1e71350576271f1e9f
James Hobin <hobinjk@gmail.com>
2022-11-29 13:55:21 -0500

    November 29 updates; Widespread renaming for consistency (#511)
    * Catalyst Trait Updates
    
    Spectacular Sphere: No more -10%
    Empowering Auras: 2% -> 3%
    
    * Willbender Trait Changes
    
    Lethal Tempo 1% -> 2%
    Tyrant's Momentum 2% -> 3%
    
    * Untamed Trait Changes
    
    Ferocious Symbiosis: 3% -> 4%
    
    * Bloody roar bugfix
    
    * Add modified warrior traits; Delete Armored Attack
    
    Merciless Hammer and Unsuspecting Foe now function with 100% uptime on
    defiant foes. Armored Attack doesn't exist any more. Stalwart Strength
    wasn't previously included.
    
    * Rename every build template to reflect outdated status
    
    * Drop asterisks in non-template presets
    
    Distributions, extras, and traits are now named without asterisks
    marking up-to-date status. This is a slight reduction in clarity
    since previously there was an implicit outdated/updated based on
    the presence of the asterisk. An alternative would be to add (Outdated)
    for a clear indicator but it felt a bit too wide and would require
    future renaming.
    
    * Mark Power Berserker as approximation
    
    Since (in theory) the only major change is the bloody roar bugfix and we
    can simulate that, mark Power Berserker as (Approx.) instead of
    (Outdated)
    
    * Mark Power Weaver as approximation
    
    Since (in theory) the only major change is the Stormsoul change and we
    can simulate that, mark Power Weaver as (Approx.) instead of (Outdated)
    
    * Update header to reflect move to use only Outdated as label
    
    * WvW trait split: Empowering Auras, Ferocious Symbiosis
    
    * Mesmer trait update
    
    Chronophantasma: 100%, PvE only
    
    * Fix: Warrior trait name typo
    
    * Improve against-defiant trait labels
    
    * oops, I can type
    
    * Guardian: Add heavy light trait
    
    * Update translation.json
    
    * Elementalist trait updates
    
     Elements of Rage: Increased damage bonus from 5% to 10% in PvE only.
     Swift Revenge: Increased damage bonus from 10% to 15% in PvE only.
    
    * Mantra of Flame
    
    Mantra of Flame: Increased burning duration from 4 seconds to 12 seconds in PvE only.
    
    * Necromancer trait updates
    
     Death Perception: Additional critical chance increased from 10% to 15% in PvE and WvW.
    
    * mark spellbreaker as outdated
    
    Breaching Strike: This skill now deals bonus damage to foes without boons
    
    Co-authored-by: Marcus <marcustyphoon@gmail.com>

--- src/pages/index/index.jsx
+++ src/pages/index/index.jsx
@@ -24,12 +24,12 @@ const IndexPage = () => {
 
   const ALERTS = [
     <Trans>
-      Core game changes are updated for the June 28, August 23, and Oct 4 game patches, but preset
-      coefficients and trait selections are not yet completely updated. Most gear results will be
-      correctly optimized, but DPS estimates and comparisons may be wrong.
+      Core game changes are updated for the November 29th game patch, but preset coefficients and
+      trait selections may not yet be completely updated. Most gear results will be correctly
+      optimized, but DPS estimates and comparisons may be wrong.
       <br />
       <br />
-      Templates with * asterisks are generated from post-patch logs and should be up to date.
+      Templates not marked as Outdated are up to date.
     </Trans>,
     <Trans>
       The gear optimizer is still being developed! Please report issues or suggest improvements in
