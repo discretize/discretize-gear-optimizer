e0a7b50f23693cae9e73abc39f7b46472ba18b46
gw2princeps <21026467+gw2princeps@users.noreply.github.com>
2023-06-29 20:34:25 +0200

    feat: add warning for beta optimizer

--- src/pages/index/index.jsx
+++ src/pages/index/index.jsx
@@ -1,17 +1,18 @@
 import { APILanguageProvider } from '@discretize/gw2-ui-new';
 import CloseIcon from '@mui/icons-material/Close';
 import GitHubIcon from '@mui/icons-material/GitHub';
-import { Collapse, IconButton, Link, Typography } from '@mui/material';
+import { Alert, Chip, Collapse, IconButton, Link, List, ListItem, Typography } from '@mui/material';
 import MuiAlert from '@mui/material/Alert';
 import React from 'react';
 import { Trans, useTranslation } from 'react-i18next';
 import { useSelector } from 'react-redux';
+import GearOptimizer from '../../components/GearOptimizer';
 import BackgroundImage from '../../components/baseComponents/BackgroundImage';
 import ErrorBoundary from '../../components/baseComponents/ErrorBoundary';
 import Layout from '../../components/baseComponents/Layout';
-import GearOptimizer from '../../components/GearOptimizer';
 import URLStateImport from '../../components/url-state/URLStateImport';
 import SagaTypes from '../../state/sagas/sagaTypes';
+import { getMulticore } from '../../state/slices/controlsSlice';
 import { getGameMode } from '../../state/slices/userSettings';
 
 // markup
@@ -19,6 +20,7 @@ const IndexPage = () => {
   const { i18n } = useTranslation();
   const { language } = i18n;
   const gameMode = useSelector(getGameMode);
+  const multicore = useSelector(getMulticore);
 
   const [alertOpen, setAlertOpen] = React.useState([true, true]);
 
@@ -84,7 +86,53 @@ const IndexPage = () => {
         ))}
         <Typography variant="h2" sx={{ paddingBottom: 2 }}>
           <Trans>Gear Optimizer</Trans>
+          {multicore && (
+            <>
+              {' '}
+              <Chip label="BETA" color="error" sx={{ alignSelf: 'flex-end' }} />
+            </>
+          )}
         </Typography>
+
+        {multicore && (
+          <Alert severity="error" sx={{ marginBottom: 2 }}>
+            <Typography variant="body2" paragraph>
+              You selected the experimental multicore mode. This mode is still in development and
+              may cause issues. Please report any issues in the Discretize Discord. More in-depth
+              explanation of the technical background can be found{' '}
+              <Link href="https://sopuli.xyz/post/713722"> here </Link>
+            </Typography>
+
+            <Typography variant="body2" paragraph>
+              Numerous features are incomplete / broken in this mode. Other than whats enumerated
+              here, the optimizer should work as expected.
+            </Typography>
+
+            <List
+              dense
+              sx={{
+                listStyleType: 'disc',
+                pl: 4,
+                '& .MuiListItem-root': {
+                  display: 'list-item',
+                },
+              }}
+            >
+              <ListItem>
+                Pausing / Resuming a calculation. Once started, the only way to prematurely
+                terminated a calculation is to refresh the tab
+              </ListItem>
+              <ListItem>
+                Infusions are not yet supported. They are ignored in the calculation
+              </ListItem>
+              <ListItem>
+                Displaying the best result for each combination is not possible due to the Rust
+                implementation not calculating every combination.
+              </ListItem>
+            </List>
+          </Alert>
+        )}
+
         <ErrorBoundary location="GearOptimizer">
           <GearOptimizer />{' '}
         </ErrorBoundary>
