44f7eec60282d233dbf8f0fd85adfa04b193cfd9
Marcus <marcustyphoon@gmail.com>
2024-04-07 22:20:03 -0700

    wip

---
+++ eslint.config.js
@@ -0,0 +1,11 @@
+import globals from 'globals';
+import eslint from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import eslintConfigPrettier from 'eslint-config-prettier';
+
+export default tseslint.config(
+  { languageOptions: { globals: { ...globals.browser, ...globals.node } } },
+  eslint.configs.recommended,
+  ...tseslint.configs.recommended,
+  eslintConfigPrettier,
+);
