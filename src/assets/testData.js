72b1dc2b28ad312e860fb3f53bbc014bd6849b42
Marcus <marcustyphoon@gmail.com>
2024-05-14 16:35:48 -0700

    Update testData.js
    [skip ci]

--- src/assets/testData.js
+++ src/assets/testData.js
@@ -303,14 +303,10 @@ const testModifiers = async () => {
         });
 
         if (modifiers.attributes || modifiers.conversion || modifiers.conversionAfterBuffs) {
-          if (temporaryBuff !== undefined) {
-            gentleAssert(
-              [true, false, 'activeOutOfCombat'].includes(temporaryBuff),
-              `err: ${id}'s temporaryBuff value is not true, false, or activeOutOfCombat!`,
-            );
-          } else {
-            gentleAssert(false, `err: ${id} is missing temporaryBuff true/false label!`);
-          }
+          gentleAssert(
+            [true, false, 'activeOutOfCombat'].includes(temporaryBuff),
+            `err: ${id}'s temporaryBuff value is not true, false, or activeOutOfCombat!`,
+          );
         }
 
         /*
