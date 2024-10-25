ca8252ec683da3f3c10f097be4b5c5e2fb83ad20
Marcus <marcustyphoon@gmail.com>
2024-10-24 19:35:35 -0700

    chore: Tweak thunk type
    [skip ci]

--- src/state/redux-hooks.ts
+++ src/state/redux-hooks.ts
@@ -16,7 +16,7 @@ export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
 export type AppThunk<ReturnType = void> = ThunkAction<
   ReturnType,
   RootState,
-  unknown,
+  undefined,
   UnknownAction
 >;
 
