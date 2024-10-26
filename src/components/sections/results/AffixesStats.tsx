9182368c1fa643ef1a465b97e51b776c054299e7
Marcus <marcustyphoon@gmail.com>
2024-10-25 17:53:09 -0700

    type affixesstats

--- src/components/sections/results/AffixesStats.tsx
+++ src/components/sections/results/AffixesStats.tsx
@@ -1,13 +1,15 @@
 import { Attribute } from '@discretize/gw2-ui-new';
 import { Table, TableBody, TableCell, TableRow, Typography } from '@mui/material';
+import type { Character } from '../../../state/optimizer/optimizerCore';
+import { objectEntries } from '../../../utils/usefulFunctions';
 
-const AffixesStats = ({ data, title }) => {
+const AffixesStats = ({ data, title }: { data: Character['gearStats']; title: string }) => {
   return (
     <>
       <Typography variant="h6">{title}</Typography>
       <Table padding="none">
         <TableBody>
-          {Object.entries(data)
+          {objectEntries(data)
             .filter(([_, value]) => value)
             .map(([attribute, value]) => (
               <TableRow hover key={attribute}>
