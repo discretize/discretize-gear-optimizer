3324fcfa7d97ee0f6ee37328d3ef6bfe3ea469b9
Marcus <marcustyphoon@gmail.com>
2024-10-25 18:03:05 -0700

    type/improve outputdistribution

--- src/components/sections/results/OutputDistribution.tsx
+++ src/components/sections/results/OutputDistribution.tsx
@@ -2,7 +2,8 @@ import { Attribute, Condition } from '@discretize/gw2-ui-new';
 import { Table, TableBody, TableCell, TableHead, TableRow, Typography } from '@mui/material';
 import { useTranslation } from 'react-i18next';
 import { makeStyles } from 'tss-react/mui';
-import { damagingConditions } from '../../../utils/gw2-data';
+import type { Character } from '../../../state/optimizer/optimizerCore';
+import { objectKeys } from '../../../utils/usefulFunctions';
 import { useAlternativeDamage2 } from '../../baseComponents/useAlternativeDamage';
 
 const useStyles = makeStyles()((theme) => ({
@@ -15,26 +16,32 @@ const useStyles = makeStyles()((theme) => ({
   },
 }));
 
-const OutputDistribution = ({ character }) => {
+const OutputDistribution = ({ character }: { character: Character }) => {
   const { classes } = useStyles();
   const { t } = useTranslation();
   const [alternativeDamageLabel] = useAlternativeDamage2(character.settings.profession);
 
   const damageLabels = {
+    Power: t('Power'),
     Power2: alternativeDamageLabel,
     Siphon: t('Life Siphon'),
   };
 
-  const { damageBreakdown = {} } = character.results;
+  if (!character.results?.damageBreakdown) return;
 
-  const data = Object.keys(damageBreakdown)
+  const { damageBreakdown } = character.results;
+
+  const data = objectKeys(damageBreakdown)
     .filter((damageType) => damageBreakdown[damageType])
-    .map((damageType) => ({
-      // Replace the names to match gw2-ui names
-      name: damageType === 'Poison' ? 'Poisoned' : damageType.replace('Damage', '').trim(),
-      value: damageBreakdown[damageType],
-      percent: (damageBreakdown[damageType] / character.attributes.Damage) * 100,
-    }));
+    .map(
+      (damageType) =>
+        ({
+          // Replace the names to match gw2-ui names
+          name: damageType === 'Poison' ? 'Poisoned' : damageType,
+          value: damageBreakdown[damageType]!,
+          percent: (damageBreakdown[damageType]! / character.attributes.Damage) * 100,
+        }) as const,
+    );
 
   return (
     <>
@@ -51,14 +58,16 @@ const OutputDistribution = ({ character }) => {
           {data.map((damageType) => (
             <TableRow hover key={damageType.name}>
               <TableCell>
-                {[...damagingConditions, 'Poisoned'].includes(damageType.name) ? (
-                  <Condition name={damageType.name} className={classes.gw2Item} />
-                ) : (
+                {damageType.name === 'Power' ||
+                damageType.name === 'Power2' ||
+                damageType.name === 'Siphon' ? (
                   <Attribute
                     name="Power"
                     className={classes.gw2Item}
                     text={damageLabels[damageType.name] ?? damageType.name}
                   />
+                ) : (
+                  <Condition name={damageType.name} className={classes.gw2Item} />
                 )}
               </TableCell>
               <TableCell align="right">{damageType.value.toFixed(1)}</TableCell>
