1f379296094680ecb42667baee366f46901edea3
marcustyphoon <marcustyphoon@gmail.com>
2025-04-18 16:28:43 -0700

    april 15 patch (#880)

--- src/pages/index/index.jsx
+++ src/pages/index/index.jsx
@@ -29,7 +29,7 @@ const IndexPage = () => {
       🎉 Janthir Wilds has been released! 🎉
       <br />
       <br />
-      Core game changes are updated for the February 11th game patch.
+      Core game changes are updated for the April 15th game patch.
     </Trans>,
     <Trans>
       The gear optimizer is still being developed! Please report issues or suggest improvements in
