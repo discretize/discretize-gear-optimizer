3dd9a5069e3ee5c74f92d77d7a662ed822eb67ad
marcustyphoon <marcustyphoon@gmail.com>
2025-12-04 02:18:52 -0800

    mark advanced uptime mods in applied modifiers section

--- src/components/baseComponents/AdvancedUptimeIndicator.tsx
+++ src/components/baseComponents/AdvancedUptimeIndicator.tsx
@@ -44,3 +44,35 @@ export default function AdvancedUptimeIndicator({
     );
   }
 }
+
+export function AppliedModifierAdvancedUptimeIndicator({
+  amountData,
+  amount,
+}: {
+  amountData?: AmountData;
+  amount?: string;
+}) {
+  const { t } = useTranslation();
+
+  if (amountData?.advancedUptimeSimulation) {
+    const { value: amountInput } = parseAmount(amount);
+    const uptime = scaleValue(1, amountInput, amountData);
+
+    const active = uptime > 0 && uptime < 1;
+
+    return (
+      <Tooltip content={active ? t('Advanced uptime simulation enabled.') : undefined}>
+        <DataSaverOnIcon
+          color={active ? 'info' : undefined}
+          fontSize="small"
+          sx={{
+            verticalAlign: 'middle',
+            marginInline: '0.5ch',
+            alignSelf: 'center',
+            opacity: active ? '1' : '0.5',
+          }}
+        />
+      </Tooltip>
+    );
+  }
+}
