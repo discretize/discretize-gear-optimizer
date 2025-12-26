a22ea56ce7d6c875032a078742f3b02df5224ece
marcustyphoon <marcustyphoon@gmail.com>
2025-12-25 21:02:24 -0800

    refactor: Remove unused NavSettings props

--- src/components/nav/NavSettings.tsx
+++ src/components/nav/NavSettings.tsx
@@ -56,23 +56,7 @@ export const GAME_MODES = (t: TFunction) => [
   { value: 'wvw', label: t('WvW') },
 ];
 
-interface NavSettingsProps {
-  disableSettings?: {
-    language?: boolean;
-    expertMode?: boolean;
-    gameMode?: boolean;
-    threading?: boolean;
-  };
-}
-
-export default function NavSettings({
-  disableSettings: {
-    language: languageDisabled,
-    expertMode: expertModeDisabled,
-    gameMode: gameModeDisabled,
-    threading: threadingDisabled,
-  } = {},
-}: NavSettingsProps) {
+export default function NavSettings() {
   const { t } = useTranslation();
   const dispatch = useAppDispatch();
   const { classes } = useStyles();
@@ -115,131 +99,120 @@ export default function NavSettings({
           Optimizer version: {__COMMIT_HASH__}
         </Typography>
       )}
+
       <Divider className={classes.divider} />
-      {!expertModeDisabled && (
-        <>
-          <FormControlLabel
-            control={
-              <Switch
-                checked={expertMode}
-                onChange={(e) => {
-                  dispatch(stopCalc);
-                  dispatch(changeExpertMode(e.target.checked));
-                }}
-                name="checked"
-                color="primary"
-              />
-            }
-            label={t('Expert')}
+
+      <FormControlLabel
+        control={
+          <Switch
+            checked={expertMode}
+            onChange={(e) => {
+              dispatch(stopCalc);
+              dispatch(changeExpertMode(e.target.checked));
+            }}
+            name="checked"
+            color="primary"
           />
-          <Divider className={classes.divider} />
-        </>
-      )}
-      {!languageDisabled && <LanguageSelection />}
+        }
+        label={t('Expert')}
+      />
 
-      {!gameModeDisabled && (
-        <>
-          <FormControl sx={{ minWidth: 150 }} size="small" variant="standard">
-            <FormLabel id="gamemode-button-group">
-              <Trans>Game Mode</Trans>
-            </FormLabel>
+      <Divider className={classes.divider} />
 
-            <RadioGroup
-              aria-labelledby="gamemode-select-label"
-              value={gameMode}
-              onChange={(e) => {
-                const newGameMode = e.target.value as GameMode;
-                dispatch(changeGameMode(newGameMode));
+      <LanguageSelection />
 
-                const isFractalsNew = newGameMode === 'fractals';
-                const isFractalsOld = gameMode === 'fractals';
-                const isFractalsChanged = isFractalsNew !== isFractalsOld;
-                if (isFractalsChanged && selectedTemplate && selectedTemplate.length > 0)
-                  setOpen(true);
-              }}
-              color="primary"
-            >
-              {GAME_MODES(t).map(({ value, label }) => (
-                <FormControlLabel key={value} value={value} control={<Radio />} label={label} />
-              ))}
-            </RadioGroup>
-          </FormControl>
-          <ReapplyTemplateDialog open={open} handleClose={handleClose} />
-        </>
-      )}
+      <FormControl sx={{ minWidth: 150 }} size="small" variant="standard">
+        <FormLabel id="gamemode-button-group">
+          <Trans>Game Mode</Trans>
+        </FormLabel>
 
-      {!threadingDisabled && (
-        <>
-          <Divider className={classes.divider} />
+        <RadioGroup
+          aria-labelledby="gamemode-select-label"
+          value={gameMode}
+          onChange={(e) => {
+            const newGameMode = e.target.value as GameMode;
+            dispatch(changeGameMode(newGameMode));
 
-          <TextField
-            label={t('Threads')}
-            helperText={t('Number of threads to use for calculations')}
-            placeholder={String(defaultHwThreads)}
-            size="small"
-            value={hwThreadsString}
-            error={hwThreadsError}
-            onChange={(e) => dispatch(changeHwThreads(e.target.value))}
-            slotProps={{
-              htmlInput: { inputMode: 'numeric', pattern: '[0-9]*' },
-              input: {
-                // used to always display the placeholder value instead of the label
-                // eslint-disable-next-line react/jsx-no-useless-fragment
-                startAdornment: <></>,
-              },
+            const isFractalsNew = newGameMode === 'fractals';
+            const isFractalsOld = gameMode === 'fractals';
+            const isFractalsChanged = isFractalsNew !== isFractalsOld;
+            if (isFractalsChanged && selectedTemplate && selectedTemplate.length > 0) setOpen(true);
+          }}
+          color="primary"
+        >
+          {GAME_MODES(t).map(({ value, label }) => (
+            <FormControlLabel key={value} value={value} control={<Radio />} label={label} />
+          ))}
+        </RadioGroup>
+      </FormControl>
+      <ReapplyTemplateDialog open={open} handleClose={handleClose} />
+
+      <Divider className={classes.divider} />
+
+      <TextField
+        label={t('Threads')}
+        helperText={t('Number of threads to use for calculations')}
+        placeholder={String(defaultHwThreads)}
+        size="small"
+        value={hwThreadsString}
+        error={hwThreadsError}
+        onChange={(e) => dispatch(changeHwThreads(e.target.value))}
+        slotProps={{
+          htmlInput: { inputMode: 'numeric', pattern: '[0-9]*' },
+          input: {
+            // used to always display the placeholder value instead of the label
+            // eslint-disable-next-line react/jsx-no-useless-fragment
+            startAdornment: <></>,
+          },
+        }}
+      />
+
+      <Divider className={classes.divider} />
+
+      <FormControlLabel
+        control={
+          <Checkbox
+            onChange={(e) => {
+              if (!enableMulticore) {
+                dispatch(stopCalc);
+              } else {
+                // workers.forEach(({ worker }) => worker.postMessage({ type: STOP }));
+                dispatch(stopCalculationParallel);
+              }
+
+              const newMulticore = e.target.checked;
+              dispatch(changeMulticore(newMulticore));
             }}
           />
-
-          <Divider className={classes.divider} />
-
-          <FormControlLabel
-            control={
-              <Checkbox
-                onChange={(e) => {
-                  if (!enableMulticore) {
-                    dispatch(stopCalc);
-                  } else {
-                    // workers.forEach(({ worker }) => worker.postMessage({ type: STOP }));
-                    dispatch(stopCalculationParallel);
-                  }
-
-                  const newMulticore = e.target.checked;
-                  dispatch(changeMulticore(newMulticore));
-                }}
-              />
-            }
-            label={t('Enable experimental Rust/WebAssembly mode')}
-            checked={enableMulticore}
+        }
+        label={t('Enable experimental Rust/WebAssembly mode')}
+        checked={enableMulticore}
+      />
+      <FormControlLabel
+        control={
+          <Checkbox
+            onChange={(e) => {
+              const newHeuristics = e.target.checked;
+              dispatch(changeHeuristics(newHeuristics));
+            }}
           />
-
-          <FormControlLabel
-            control={
-              <Checkbox
-                onChange={(e) => {
-                  const newHeuristics = e.target.checked;
-                  dispatch(changeHeuristics(newHeuristics));
-                }}
-              />
-            }
-            label={t('Enable heuristics')}
-            sx={{ display: 'none' }}
-            checked={enableHeuristics}
+        }
+        label={t('Enable heuristics')}
+        sx={{ display: 'none' }}
+        checked={enableHeuristics}
+      />
+      <FormControlLabel
+        control={
+          <Checkbox
+            onChange={(e) => {
+              dispatch(changeIncludeScenarioDataInCharacters(e.target.checked));
+            }}
           />
-
-          <FormControlLabel
-            control={
-              <Checkbox
-                onChange={(e) => {
-                  dispatch(changeIncludeScenarioDataInCharacters(e.target.checked));
-                }}
-              />
-            }
-            label={t('Include scenario data in internal character JSON')}
-            slotProps={{ typography: { variant: 'caption' } }}
-            checked={includeScenarioDataInCharacters}
-          />
-        </>
-      )}
+        }
+        label={t('Include scenario data in internal character JSON')}
+        slotProps={{ typography: { variant: 'caption' } }}
+        checked={includeScenarioDataInCharacters}
+      />
     </Settings>
   );
 }
