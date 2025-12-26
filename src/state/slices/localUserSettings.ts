a3dde91b859ced4d10a9e74d8ab0d3ccb1f87ef0
marcustyphoon <marcustyphoon@gmail.com>
2025-12-26 01:06:39 -0800

    chore: Refactor user settings slice

--- src/state/slices/localUserSettings.ts
+++ src/state/slices/localUserSettings.ts
@@ -2,29 +2,18 @@ import type { PayloadAction } from '@reduxjs/toolkit';
 import { createSlice } from '@reduxjs/toolkit';
 import { reduxSideEffect } from '../redux-hooks';
 import type { RootState } from '../store';
+import { getLocalStorageObject } from '../../utils/usefulFunctions';
 
 // this slice is for settings which should not be loaded from URLs;
 // it has no changeAll reducer case
 
 const UNSHARED_SETTINGS_STORAGE_KEY = 'localUserSettings';
 
-function getLocalStorageState(): object {
-  try {
-    const stored = localStorage.getItem(UNSHARED_SETTINGS_STORAGE_KEY);
-    if (stored) {
-      return JSON.parse(stored) as object;
-    }
-  } catch {
-    return {};
-  }
-  return {};
-}
-
 const defaultState = { defaultStatInfusions: '' };
 
 export const loadedLocalUserSettings = {
   ...defaultState,
-  ...getLocalStorageState(), // override default state with potentially saved localStorage variables
+  ...getLocalStorageObject(UNSHARED_SETTINGS_STORAGE_KEY), // override default state with potentially saved localStorage variables
 };
 
 const getSliceState = (state: RootState) => state.optimizer.localUserSettings;
--- src/state/slices/userSettings.ts
+++ src/state/slices/userSettings.ts
@@ -1,22 +1,14 @@
 import type { PayloadAction } from '@reduxjs/toolkit';
 import { createSlice } from '@reduxjs/toolkit';
+import React from 'react';
+import { useTranslation } from 'react-i18next';
+import { useSelector } from 'react-redux';
 import { PARAMS, setQueryParm, useQueryParam } from '../../utils/queryParam';
+import { getLocalStorageObject } from '../../utils/usefulFunctions';
 import type { RootState } from '../store';
 import { changeAll } from './controlsSlice';
 
 export type GameMode = 'fractals' | 'raids' | 'wvw';
-
-function getLocalStorageState(): object {
-  try {
-    const stored = localStorage.getItem(SETTINGS_STORAGE_KEY);
-    if (stored) {
-      return JSON.parse(stored) as object;
-    }
-  } catch {
-    return {};
-  }
-  return {};
-}
 
 const SETTINGS_STORAGE_KEY = 'globalSettings';
 
@@ -24,9 +16,10 @@ const defaultState = { expertMode: true, gameMode: 'fractals', language: 'en' };
 // eslint-disable-next-line react-hooks/rules-of-hooks
 const gameModeParam = useQueryParam({ key: PARAMS.GAMEMODE });
 
+// language is included here for historical reasons; src/utils/i18n.js imports this
 export const loadedSettings = {
   ...defaultState,
-  ...getLocalStorageState(), // override default state with potentially saved localStorage variables
+  ...getLocalStorageObject(SETTINGS_STORAGE_KEY), // override default state with potentially saved localStorage variables
   ...(gameModeParam && { gameMode: gameModeParam }), // gameMode from query param takes priority
 } as {
   expertMode: boolean;
@@ -37,18 +30,31 @@ export const loadedSettings = {
 // append the gamemode to the query param if no query param is present
 if (!gameModeParam) setQueryParm({ key: PARAMS.GAMEMODE, value: loadedSettings.gameMode });
 
+// save user settings to localStorage (as a react hook, to access react-i18next language)
+const getSliceState = (state: RootState) => state.optimizer.userSettings;
+export const useSaveUserSettingsOnChange = () => {
+  const { i18n } = useTranslation();
+  const { language } = i18n;
+  const sliceState = useSelector(getSliceState);
+
+  React.useEffect(() => {
+    const settings = JSON.stringify({ ...sliceState, language });
+    console.log(`saving... ${settings}`);
+
+    localStorage.setItem(SETTINGS_STORAGE_KEY, settings);
+  }, [sliceState, language]);
+};
+
 export const userSettingsSlice = createSlice({
   name: 'userSettings',
 
   initialState: {
     expertMode: loadedSettings.expertMode,
     gameMode: loadedSettings.gameMode,
+    // language is excluded from this redux slice; react-i18next has its own mutable state
   },
 
   reducers: {
-    changeAllUserSettings: (state, action) => {
-      return { ...state, ...action.payload?.userSettings };
-    },
     changeExpertMode: (state, action: PayloadAction<boolean>) => {
       state.expertMode = action.payload;
     },
@@ -74,7 +80,6 @@ export const userSettingsSlice = createSlice({
 export const getExpertMode = (state: RootState) => state.optimizer.userSettings.expertMode;
 export const getGameMode = (state: RootState) => state.optimizer.userSettings.gameMode;
 
-export const { changeAllUserSettings, changeExpertMode, changeGameMode } =
-  userSettingsSlice.actions;
+export const { changeExpertMode, changeGameMode } = userSettingsSlice.actions;
 
 export default userSettingsSlice.reducer;
